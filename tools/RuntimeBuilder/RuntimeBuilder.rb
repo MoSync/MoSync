1291711b9a2f397baa588604d8fdaded9ece5e64
nummelin <nummelin@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2009-09-30 09:42:14 +0000

    MoSync, tools, added RuntimeBuilder
    git-svn-id: svn://localhost/public/MoSync/trunk@14 6ad7a17e-d552-4a1e-ace4-f958fe14b117

---
+++ tools/RuntimeBuilder/RuntimeBuilder.rb
@@ -0,0 +1,37 @@
+# Copyright (C) 2009 Mobile Sorcery AB
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License, version 2, as published by
+# the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to the Free
+# Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+# usage: ruby RuntimeBuilder.rb <Settings.rb file> <platform> <runtime directory (containing config.h)>
+
+class RuntimeBuilder 	
+	def method_missing(method, *args) 
+		puts "RuntimeBuilder failed: invalid platform " + method.to_s
+	end
+
+	def build(platform, runtime_dir)		
+		puts "building " + platform
+		send(platform, runtime_dir)
+	end
+
+end
+
+require ARGV[0]
+include "BuildJavaME.rb"
+include "BuildS60.rb"
+include "BuildWinCE.rb"
+
+runtime_builder = RuntimeBuilder.new
+runtime_builder.build(ARGV[1], ARGV[2])
