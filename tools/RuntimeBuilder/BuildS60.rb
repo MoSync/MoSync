57791fdafd6dd8c77373e0fce9dd9109625a3f40
anders <anders@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2009-09-30 11:17:13 +0000

    added files, rename part 2
    git-svn-id: svn://localhost/public/MoSync/trunk@18 6ad7a17e-d552-4a1e-ace4-f958fe14b117

---
+++ tools/RuntimeBuilder/BuildS60.rb
@@ -0,0 +1,39 @@
+# Copyright (C) 2009 Mobile Sorcery AB
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License, version 2, as published by
+# the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to the Free
+# Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+class runtimeBuilder
+
+	def s60v2(runtime_dir)
+
+		# Copy the old config_platform.h file and copy the one from the runtime_dir to the source location
+		
+		# Run the make file
+		
+		# Copy the result to the runtime dir
+	
+	end
+	
+	def s60v3(runtime_dir)
+	
+		# Copy the old config_platform.h file and copy the one from the runtime_dir to the source location
+		
+		# Run the make file
+		
+		# Copy the result to the runtime dir
+	
+	end
+
+end
