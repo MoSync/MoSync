3f17eaf96640d96584a0562a3c1dd0f0db7ba9c4
anders <anders@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2009-10-06 09:18:02 +0000

    Fixed a line in host.rb.. It checks the version on gcc but builds with xgcc. The only gcc I have is the symbian one and that doens't work so it just crashes.. This must be fixed!
    git-svn-id: svn://localhost/public/MoSync/trunk@70 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- build_rules/host.rb
+++ build_rules/host.rb
@@ -62,12 +62,13 @@ else
 	error("Unknown platform: #{UNAME}")
 end
 
-GCC_VERSION = (open("|gcc --version").readline().split(/ /))[2]
+GCC_VERSION = (open("|xgcc --version").readline().split(/ /))[2]
 GCC_IS_V4 = (GCC_VERSION[0] >= "4"[0])
 GCC_IS_V43 = (GCC_IS_V4 && (GCC_VERSION[2] >= "3"[0]))
 
 # debugging
 #warning("Platform: #{HOST}")
+
 #warning("GCC version: #{GCC_VERSION}")
 #warning("GCC_IS_V4: #{GCC_IS_V4}")
 #warning("GCC_IS_V43: #{GCC_IS_V43}")
