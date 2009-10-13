c08cd429dbebab31128e827b8ac62b460b433ee0
anders <anders@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2009-10-13 14:20:48 +0000

    added a new enviroment path to fix problems with the doc generator
    git-svn-id: svn://localhost/public/MoSync/trunk@142 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- tools/ReleasePackageBuild/build_package.bat
+++ tools/ReleasePackageBuild/build_package.bat
@@ -64,9 +64,8 @@ REM Usage: build_package.bat c:\SonyEricsson\JavaME_SDK_CLDC\OnDeviceDebug\ 2>&1
 @SET MOSYNC_PROFILES_PATH=%MOSYNC_PATH%\profiles
 @SET MOSYNC_TRUNK=%MOSYNC_RELEASE_BUILD_PATH%\mosync-trunk
 
-@REM We fix this path since one of the tools needs the C:\ in the path
 cd %MOSYNC_PATH%
-@SET MOSYNCDIR=%CD%
+@SET FULLPATH_MOSYNCDIR=%CD%
 cd %ORIGINAL_PATH%
 
 @IF "%2" == "/SKIP" goto MKDIRS
