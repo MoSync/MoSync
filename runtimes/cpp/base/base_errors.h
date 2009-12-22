cdcce630d9cf8e72a5a3fb3ea6c70df7ffa51eb7
patrick <patrick@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2009-12-22 09:43:18 +0000

    Fixed panic code value inconsistency introduced in r216 by adding a panic code in the middle of the existing list.
    git-svn-id: svn://localhost/public/MoSync/trunk@433 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- runtimes/cpp/base/base_errors.h
+++ runtimes/cpp/base/base_errors.h
@@ -24,7 +24,6 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 	m(ERR_RES_INVALID_INDEX, "Resource handle out of bounds")\
 	m(ERR_RES_OVERWRITE, "Attempted to overwrite an existing resource")\
 	m(ERR_RES_LOAD_UBIN, "Attempted to load a UBIN from memory")\
-	m(ERR_RES_NO_RESOURCES, "Attempted to load resource, but no resources exist.")\
 	m(ERR_DATA_OOB, "Data object access out of bounds")\
 	m(ERR_RES_FILE_INCONSISTENT, "Resource file inconsistent")\
 	m(ERR_DIVISION_BY_ZERO, "Division by zero")\
@@ -73,7 +72,8 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 	m(ERR_MEMORY_PROTECTED, "Attempted access protected memory")\
 	m(ERR_IMAGE_SIZE_INVALID, "Invalid image size")\
 	m(ERR_MEMORY_NULL, "Attempted to access null pointer.")\
-	m(ERR_GDB_SERVER_OPEN, "Gdb stub failed to open listening server socket.")
+	m(ERR_GDB_SERVER_OPEN, "Gdb stub failed to open listening server socket.")\
+	m(ERR_RES_NO_RESOURCES, "Attempted to load resource, but no resources exist.")
 
 DECLARE_ERROR_ENUM(BASE)
 
