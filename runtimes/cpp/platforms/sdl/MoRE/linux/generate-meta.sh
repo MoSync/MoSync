aa48006e3f4ae82e560f196fca7ffbe58cb85f2c
ali <ali@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-01-16 22:59:46 +0000

    Summary: More fixes for the template package builder.
    git-svn-id: svn://localhost/public/MoSync/trunk@516 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- runtimes/cpp/platforms/sdl/MoRE/linux/generate-meta.sh
+++ runtimes/cpp/platforms/sdl/MoRE/linux/generate-meta.sh
@@ -24,9 +24,14 @@ if [ "$requires" = "" ]; then
 	fi
 fi	
 
-echo "{
+echo -e "{
+	\"prerm\"  	  : \".meta/prerm\",
+	\"postrm\"    : \".meta/postrm\",
+	\"preinst\"   : \".meta/preinst\",
 	\"postinst\"  : \".meta/postinst\",
 	\"programDir\": \"usr/local/%appname%/bin\",
+	\"programFile\": \"usr/local/%appname%/bin/program\",
+	\"resourceFile\": \"usr/local/%appname%/bin/resources\",
 	\"parseList\" : [ \"bin/%appname%\", \"usr/share/applications/%appname%.desktop\" ],
 	\"iconList\"  : [ {\"type\": \"png\", \"size\":16, \"path\":\"usr/share/icons/hicolor/16x16/apps/%appname%.png\"}, {\"type\": \"png\", \"size\":32, \"path\":\"usr/share/icons/hicolor/32x32/apps/%appname%.png\"}, {\"type\": \"png\", \"size\":48, \"path\":\"usr/share/icons/hicolor/48x48/apps/%appname%.png\"}, {\"type\": \"png\", \"size\":64, \"path\":\"usr/share/icons/hicolor/64x64/apps/%appname%.png\"}, {\"type\": \"png\", \"size\":128, \"path\":\"usr/share/icons/hicolor/128x128/apps/%appname%.png\"}, {\"type\": \"png\", \"size\":256, \"path\":\"usr/share/icons/hicolor/256x256/apps/%appname%.png\"}, {\"type\": \"svg\", \"path\":\"usr/share/icons/hicolor/scalable/apps/%appname%.svg\"} ]
 	$depends
