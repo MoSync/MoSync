bd7a12d293a7581fe3aa1632406a139c7cdf9859
fredrik <fredrik@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-02-05 10:48:53 +0000

    Tutorials: added DataHandler and PlaceholderPool.
    git-svn-id: svn://localhost/public/MoSync/trunk@648 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- tools/DocbookIndexer/run.bat
+++ tools/DocbookIndexer/run.bat
@@ -2,3 +2,4 @@ REM call setjavacpath.bat
 set BASEDIR=%CD%
 cd src\com\mobilesorcery\mosync\docbook\
 call buildrun.bat %BASEDIR%\..\ReleasePackageBuild\build_package_tools\bin\docbookParser-lib %1
+cd ..
