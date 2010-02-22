93d5c150a0b124b026478536c09807c80c603848
fredrik <fredrik@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-02-22 09:05:44 +0000

    Trunk, MAUtil: fixed HashMap multiple definitions.
    git-svn-id: svn://localhost/public/MoSync/trunk@791 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- libs/MAUtil/HashMap_impl.h
+++ libs/MAUtil/HashMap_impl.h
@@ -20,25 +20,6 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #endif
 
 //******************************************************************************
-// THashFunction
-//******************************************************************************
-
-template<> hash_val_t MAUtil::THashFunction<MAUtil::String>(const MAUtil::String& s) {
-	return hash_fun_default(s.c_str());
-}
-
-template<> hash_val_t MAUtil::THashFunction<int>(const int& data) {
-	int key = data;
-	key = key + ~(key << 15); // key = key - (key << 15) - 1;
-	key = key ^ (key >> 10);
-	key = key + (key << 3);
-	key = key ^ (key >> 6);
-	key = key * 16389; // key = (key + (key << 2)) + (key << 14);
-	key = key ^ (key >> 16);
-	return key;
-}
-
-//******************************************************************************
 // HashMap
 //******************************************************************************
 
