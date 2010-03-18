adf1622afe561eea330121364dc866adffdccf99
fredrik <fredrik@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-03-18 11:01:29 +0000

    Trunk: added more Unicode characters to testProgram/wsprintf.
    git-svn-id: svn://localhost/public/MoSync/trunk@905 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- testPrograms/wsprintf/main.c
+++ testPrograms/wsprintf/main.c
@@ -1,9 +1,15 @@
 #include <mawvsprintf.h>
+#include <maapi.h>
+#include <maassert.h>
 
 int MAMain() {
-	const wchar* str = L"Hello World!";
+	const wchar* str = L"H 蝮 㦩햭Ｑӭܨপ૩ిฐະშኡᎣᑷᾨⅦ⿏タ World!";
 	wchar buf[256];
-	wsprintf(buf, L"Unicode %s", str);
+	wsprintf(buf, L"U%s", str);
 	wlprintfln(L"%s", buf);
+	maSetColor(0xFFFFFF);
+	maDrawTextW(10, 10, buf);
+	maUpdateScreen();
+	FREEZE;
 	return 0;
 }
