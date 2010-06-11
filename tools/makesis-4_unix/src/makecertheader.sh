771a3d4121299d020967f7588a0b4840a6e3bc0c
fredrik <fredrik@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-06-11 15:32:16 +0000

    Trunk, makesis-unix: cleanup.
    git-svn-id: svn://localhost/public/MoSync/trunk@1458 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- tools/makesis-4_unix/src/makecertheader.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "static const char selfsignedCer[] = " > selfsigned.cer.h
-cat selfsigned.cer | sed 's/^/"/' | sed 's/$/\\n"/' >> selfsigned.cer.h
-echo ";" >> selfsigned.cer.h
-echo "static const char selfsignedKey[] = " > selfsigned.key.h
-cat selfsigned.key | sed 's/^/"/' | sed 's/$/\\n"/' >> selfsigned.key.h
-echo ";" >> selfsigned.key.h
-
