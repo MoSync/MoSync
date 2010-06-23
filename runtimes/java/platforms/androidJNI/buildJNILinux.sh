6b558e03f625b57c30c3ddad94508c62b0de868b
anders <anders@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-06-23 10:30:13 +0000

    trunk: Shell file to build the JNI runtime on Linux/OS X
    git-svn-id: svn://localhost/public/MoSync/trunk@1526 6ad7a17e-d552-4a1e-ace4-f958fe14b117

---
+++ runtimes/java/platforms/androidJNI/buildJNILinux.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# These should be defined outside of this file!
+export MOSYNC_SRC="/Users/anders/Code/trunk"
+export ANDROID_PATH="/Users/anders/Android/android-ndk-r4"
+
+export MOSYNC_CPP=$MOSYNC_SRC"/runtimes/cpp"
+export MOSYNC_CPP_SRC=$MOSYNC_SRC"/runtimes/cpp/platforms/android"
+export MOSYNC_JAVA_SRC=$MOSYNC_SRC"/runtimes/java/platforms/androidJNI"
+
+if [ -d $MOSYNC_JAVA_SRC"/AndroidProject/jni" ]; then
+	echo "jni folder exists!"
+else
+	echo "jni folder doesn't exist, I'll add that for you!"
+	mkdir $MOSYNC_JAVA_SRC"/AndroidProject/jni"
+fi
+
+cp $MOSYNC_CPP_SRC"/Application.mk" $MOSYNC_JAVA_SRC"/AndroidProject/jni"
+cp $MOSYNC_CPP_SRC"/Android.mk" $MOSYNC_JAVA_SRC"/AndroidProject/jni"
+
+cd $MOSYNC_JAVA_SRC"/src"
+cp config_platform.h.example $MOSYNC_CPP_SRC"/config_platform.h"
+cp config_platform.h.example $MOSYNC_CPP_SRC"/config_platform.h"
+
+echo "Build library!"
+
+cd $MOSYNC_JAVA_SRC/AndroidProject
+
+$ANDROID_PATH/ndk-build
+
+echo "Finished!"
