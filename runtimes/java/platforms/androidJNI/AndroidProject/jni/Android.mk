98e0008e3dc923ad8961e687d774ef98048f6152
anders <anders@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-06-29 23:23:33 +0000

    trunk: moved the JNI make files
    git-svn-id: svn://localhost/public/MoSync/trunk@1569 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- runtimes/java/platforms/androidJNI/AndroidProject/jni/Android.mk
+++ runtimes/java/platforms/androidJNI/AndroidProject/jni/Android.mk
@@ -1,19 +1,28 @@
-# Copyright (C) 20010 MoSync AB
-# 
+# Copyright (C) 2010 Mobile Sorcery AB
+#
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License, version 2, as published by
 # the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 # for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; see the file COPYING.  If not, write to the Free
 # Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 
-APP_PROJECT_PATH := $(MOSYNC_SRC)/runtimes/java/platforms/androidJNI/AndroidProject
-APP_MODULES      := mosync
-APP_CPPFLAGS := -D_android -I$(MOSYNC_CPP)/core -I$(MOSYNC_CPP)/base -I$(MOSYNC_CPP) -I$(MOSYNC_CPP_SRC) -I$(MOSYNC_SRC)/intlibs
+LOCAL_PATH:= /cygdrive/d/android-ndk-1.5_r1/sources/mosync
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := mosync
+LOCAL_ARM_MODE  := arm
+LOCAL_SRC_FILES := Core.cpp FileImpl.cpp FileStream.cpp MemStream.cpp Stream.cpp Image.cpp hashmap/hashmap.cpp AndroidHelpers.cpp Syscall.cpp SyscallImpl.cpp IOCtl.cpp disassembler.cpp Recompiler/ArmAssembler.cpp Recompiler/ArmRecompiler.cpp MoSyncBridge.cpp helpers/platforms/linux/log.cpp
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+
+LOCAL_CFLAGS := -DFPM_ARM
+
+include $(BUILD_SHARED_LIBRARY)
