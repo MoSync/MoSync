d442898967a600f164e3a94ee536bf70c33cd125
fredrik <fredrik@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-07-14 09:14:33 +0000

    Trunk, gcc-torture: Fixed settings.example.rb.
    git-svn-id: svn://localhost/public/MoSync/trunk@1642 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- testPrograms/gcc-torture/settings.example.rb
+++ testPrograms/gcc-torture/settings.example.rb
@@ -1,5 +1,6 @@
 SETTINGS = {
-	:source_path => '/gcc-3.4.6/gcc/testsuite/gcc.c-torture/execute'
-	:stop_on_fail => true
-	:rebuild_failed => true
+	:source_path => '/gcc-3.4.6/gcc/testsuite/gcc.c-torture/execute',
+	:stop_on_fail => true,
+	:rebuild_failed => true,
+	:retry_failed => true,
 }
