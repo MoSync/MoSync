MoSync/MoSync

commit b743e567aaaf4f671a2f8c95c9ab4be0f211c99a

Trunk, newlib: implemented dup, dup2, ftruncate and creat. Added stubs for some more functions. Added a script to run the glibc test suite on MoSync/newlib. At the moment, it's incomplete and most tests that compile, fail when run.
git-svn-id: svn://localhost/public/MoSync/trunk@1644 6ad7a17e-d552-4a1e-ace4-f958fe14b117

libs/newlib/libc/sys/mosync/machine.c
@@ -7,6 +7,12 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <inttypes.h>
+#include <pwd.h>
+#include <grp.h>
+#include <utime.h>
+#include <sys/signal.h>
+#include <string.h>
+#include <getopt.h>
 
 #include <maapi.h>
 #include <IX_FILE.h>
@@ -44,77 +50,220 @@ int getpid(void) {
   return 1;
 }
 
+uid_t getuid(void) {
+	return 0;
+}
+
+struct passwd* getpwuid(uid_t uid) {
+	return NULL;
+}
+
+struct group* getgrgid(gid_t gid) {
+	return NULL;
+}
+
 int fstat(int __fd, struct stat *st) {
 	st->st_mode = S_IFCHR;
 	return 0;
 }
+
 int stat(const char *file, struct stat *st) {
 	st->st_mode = S_IFCHR;
 	return 0;
 }
 
-#define FHANDLE_OFFSET 4
+// Define file descriptors.
+// In this scheme, a file descriptor is an index into this array.
+// Each value in this array is a pointer to a struct, which is stored in yet another array.
+// This struct has a low-level descriptor, which means this:
+// 0 is invalid. 1 is the console. 2 is maWriteLog().
+// 3 and above are file handles, as returned by maFileOpen(), plus LOWFD_OFFSET.
+// With this scheme, dup() and dup2() should be possible to implement.
+// The struct also has a reference counter.
 
-off_t lseek(int __fd, off_t __offset, int __whence) {
-	int res;
-	if(__fd <= FHANDLE_OFFSET) {
+#define LOWFD_CONSOLE 0
+#define LOWFD_WRITELOG 1
+#define LOWFD_OFFSET 2
+
+struct LOW_FD {
+	int lowFd;
+	int refCount;
+};
+
+#define NFD 32
+static struct LOW_FD* sFda[NFD];
+static struct LOW_FD sLfda[NFD];
+
+static struct LOW_FD sLfConsole = { LOWFD_CONSOLE, 1 };
+//static struct LOW_FD sLfWriteLog = { LOWFD_WRITELOG, 1 };
+
+static int closeLfd(struct LOW_FD* plfd);
+
+static void initFda(void) {
+	static int initialized = 0;
+	if(initialized)
+		return;
+	initialized = 1;
+
+	memset(sFda, 0, sizeof(sFda));
+	memset(sLfda, 0, sizeof(sLfda));
+	sFda[1] = &sLfConsole;
+	sFda[2] = &sLfConsole;
+	sFda[3] = &sLfConsole;
+}
+
+static struct LOW_FD* getLowFd(int __fd) {
+	initFda();
+	if(__fd <= 0 || __fd >= NFD) {
+		errno = EBADF;
+		return NULL;
+	}
+	if(sFda[__fd] == NULL) {
+		errno = EBADF;
+		return NULL;
+	}
+	MAASSERT(sFda[__fd]->refCount > 0);
+	return sFda[__fd];
+}
+#define LOWFD int lfd; struct LOW_FD* plfd = getLowFd(__fd); if(plfd == NULL) return -1; lfd = plfd->lowFd;
+
+static struct LOW_FD* findFreeLfd(void) {
+	initFda();
+	for(int i=0; i<NFD; i++) {
+		if(sLfda[i].refCount == 0)
+			return sLfda + i;
+	}
+	BIG_PHAT_ERROR;
+}
+
+static int findFreeFd(void) {
+	initFda();
+	for(int i=4; i<NFD; i++) {
+		if(sFda[i] == NULL)
+			return i;
+	}
+	return -1;
+}
+
+
+int dup(int __fd) {
+	int newFd;
+	LOWFD;
+	newFd = findFreeFd();
+	if(newFd < 0) {
+		errno = EMFILE;
+		return -1;
+	}
+	sFda[newFd] = plfd;
+	plfd->refCount++;
+	return newFd;
+}
+
+int dup2(int __fd, int newFd) {
+	struct LOW_FD* newLfd;
+	LOWFD;
+	if(__fd == newFd)
+		return newFd;
+	if(newFd < 0 || newFd >= NFD) {
 		errno = EBADF;
 		return -1;
 	}
-	res = maFileSeek(__fd - FHANDLE_OFFSET, __offset, __whence);
+	newLfd = getLowFd(newFd);
+	if(newLfd != NULL) {
+		int res = closeLfd(newLfd);
+		if(res < 0)
+			return res;
+	}
+	sFda[newFd] = plfd;
+	plfd->refCount++;
+	return newFd;
+}
+
+
+off_t lseek(int __fd, off_t __offset, int __whence) {
+	int res;
+	LOWFD;
+	res = maFileSeek(lfd - LOWFD_OFFSET, __offset, __whence);
 	if(res < 0) {
 		errno = EINVAL;
 		return -1;
 	}
 	return res;
 }
-int read(int __fd, void *__buf, size_t __nbyte) {
+
+int ftruncate(int __fd, off_t __length) {
 	int res;
-	if(__fd <= FHANDLE_OFFSET) {
-		errno = EBADF;
+	LOWFD;
+	res = maFileTruncate(lfd - LOWFD_OFFSET, __length);
+	if(res < 0) {
+		errno = EINVAL;
 		return -1;
 	}
-	res = maFileRead(__fd - FHANDLE_OFFSET, __buf, __nbyte);
+	return res;
+}
+
+int read(int __fd, void *__buf, size_t __nbyte) {
+	int res;
+	LOWFD;
+	res = maFileRead(lfd - LOWFD_OFFSET, __buf, __nbyte);
 	if(res != 0) {
 		errno = EIO;
 		return -1;
 	}
 	return __nbyte;
 }
+
 int write(int __fd, const void *__buf, size_t __nbyte) {
-	if(__fd == 1 || __fd == 3) {	//stdout or stderr
+	int res;
+	LOWFD;
+	if(lfd == LOWFD_CONSOLE) {
 		WriteConsole(__buf, __nbyte);
 		return __nbyte;
-	} else if(__fd > FHANDLE_OFFSET) {
-		int res = maFileWrite(__fd - FHANDLE_OFFSET, __buf, __nbyte);
-		if(res < 0) {
-			errno = EIO;
-			return -1;
-		}
-		return __nbyte;
+	} else if(lfd == LOWFD_WRITELOG) {
+		res = maWriteLog(__buf, __nbyte);
 	} else {
-		lprintfln("write(%i, %p, %zu)", __fd, __buf, __nbyte);
-		NOT;
+		res = maFileWrite(lfd - LOWFD_OFFSET, __buf, __nbyte);
 	}
-}
-int close(int __fd) {
-	int res;
-	if(__fd <= FHANDLE_OFFSET) {
-		errno = EBADF;
-		return -1;
-	}
-	res = maFileClose(__fd - FHANDLE_OFFSET);
-	if(res != 0) {
+	if(res < 0) {
 		errno = EIO;
 		return -1;
 	}
+	return __nbyte;
+}
+
+static int closeLfd(struct LOW_FD* plfd) {
+	int res;
+	plfd->refCount--;
+	if(plfd->refCount == 0) {
+		res = maFileClose(plfd->lowFd - LOWFD_OFFSET);
+		if(res != 0) {
+			errno = EIO;
+			return -1;
+		}
+	}
 	return 0;
 }
+
+int close(int __fd) {
+	int res;
+	LOWFD;
+	res = closeLfd(plfd);
+	sFda[__fd] = NULL;
+	return res;
+}
+
+int isatty(int __fd) {
+	LOWFD;
+	return lfd == LOWFD_CONSOLE;
+}
+
 int open(const char * __filename, int __mode, ...) {
 	int ma_mode;
 	int res;
 	int handle;
 	int exists;
+	int newFd;
+	struct LOW_FD* newLfd;
 	if(__mode & O_WRONLY) {
 		ma_mode = MA_ACCESS_READ_WRITE;
 	} else if(__mode & O_RDONLY) {
@@ -129,6 +278,14 @@ int open(const char * __filename, int __mode, ...) {
 		PANIC_MESSAGE("unsupported mode: O_APPEND");
 	}
 	// O_SHLOCK, O_EXLOCK and O_NOATIME are unsupported. we drop them silently.
+
+	// Find a spot in the descriptor array.
+	newFd = findFreeFd();
+	if(newFd < 0) {
+		errno = EMFILE;
+		return -1;
+	}
+	newLfd = findFreeLfd();
 
 	res = maFileOpen(__filename, ma_mode);
 	if(res < 0) {
@@ -168,23 +325,33 @@ int open(const char * __filename, int __mode, ...) {
 			return -1;
 		}
 	}
-	return handle + FHANDLE_OFFSET;
+	sFda[newFd]->lowFd = handle + LOWFD_OFFSET;
+	sFda[newFd]->refCount = 1;
+	return newFd;
 }
+
+int creat(const char *filename, mode_t mode) {
+	return open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+}
+
 int unlink(const char *name) {
 	int res;
-	int handle;
-	handle = open(name, O_WRONLY);
-	if(handle < 0)
-		return handle;
-	res = maFileDelete(handle - FHANDLE_OFFSET);
+	int __fd;
+	__fd = open(name, O_WRONLY);
+	if(__fd < 0)
+		return __fd;
+	res = maFileDelete(sFda[__fd]->lowFd - LOWFD_OFFSET);
 	if(res < 0) {
 		errno = EPERM;
 		return -1;
 	}
 	return 0;
 }
-int isatty(int __fd) {
-	return __fd >= 1 && __fd <= 3;
+
+
+int chdir(const char *filename) {
+	errno = ENOSYS;
+	return -1;
 }
 
 char* getenv(const char *__string) {
@@ -195,9 +362,43 @@ char* _getenv_r(struct _reent *p, const char *__string) {
 	return NULL;
 }
 
+int setenv(const char *name, const char *value, int replace) {
+	errno = ENOSYS;
+	return -1;
+}
+
+int setpgid(pid_t pid, pid_t pgid) {
+	errno = ENOSYS;
+	return -1;
+}
+
+int fork(void) {
+	errno = ENOSYS;
+	return -1;
+}
+
+_sig_func_ptr signal(int sig, _sig_func_ptr f) {
+	errno = ENOSYS;
+	return SIG_ERR;
+}
+
+unsigned int alarm(unsigned int seconds) {
+	BIG_PHAT_ERROR;
+}
+
+
+int mallopt(int parameter, int value) {
+	errno = ENOSYS;
+	return 0;
+}
+
+int getpagesize(void) {
+	return 1024;	//arbitrary
+}
 
 
 clock_t times (struct tms *buffer) {
+	errno = ENOSYS;
 	return -1;
 }
 

libs/newlib/libc/include/machine/endian.h
@@ -17,4 +17,8 @@
 #endif
 #endif
 
+#define __BIG_ENDIAN BIG_ENDIAN
+#define __LITTLE_ENDIAN LITTLE_ENDIAN
+#define __BYTE_ORDER BYTE_ORDER
+
 #endif /* __MACHINE_ENDIAN_H__ */

libs/newlib/libc/include/sys/unistd.h
@@ -215,7 +215,7 @@ _READ_WRITE_RETURN_TYPE _EXFUN(_write, (int __fd, const void *__buf, size_t __nb
 int     _EXFUN(_execve, (const char *__path, char * const __argv[], char * const __envp[] ));
 #endif
 
-#if defined(__CYGWIN__) || defined(__rtems__) || defined(__sh__) || defined(__SPU__)
+#if defined(__CYGWIN__) || defined(__rtems__) || defined(__sh__) || defined(__SPU__) || defined(MAPIP)
 #if !defined(__INSIDE_CYGWIN__)
 int     _EXFUN(ftruncate, (int __fd, off_t __length));
 int     _EXFUN(truncate, (const char *, off_t __length));

tests/libc-suite/all_tests.rb
@@ -1,0 +1,188 @@
+#!/usr/bin/ruby
+
+require 'FileUtils'
+require 'settings.rb'
+require 'skipped.rb'
+require '../../rules/util.rb'
+
+BUILD_DIR = 'build'
+MOSYNCDIR = ENV['MOSYNCDIR']
+GCC_FLAGS = " -I- -std=gnu99 -I. -I#{MOSYNCDIR}/include/newlib -I \"#{SETTINGS[:source_path][0..-2]}\" -DNO_TRAMPOLINES -include skeleton.h"
+PIPE_FLAGS = " -datasize=#{2*1024*1024} -stacksize=#{512*1024} -heapsize=#{1024*1024}"
+PIPE_LIBS = " build/helpers.s #{MOSYNCDIR}/lib/newlib_debug/newlib.lib"
+
+# SETTINGS[:source_path] - directory in which source files are stored.
+
+# use Work to build the tests?
+# build them directly with command-line gcc & pipe-tool. no dependency or flags handling this way,
+# but it would be faster.
+
+FileUtils.mkdir_p(BUILD_DIR)
+
+sh "#{MOSYNCDIR}/bin/xgcc -g -S helpers.c -o build/helpers.s#{GCC_FLAGS}"
+
+# Find tests.
+# We have many directories. Some of these have Makefiles with a definition of a "tests" variable.
+# This variable is a list of names of test programs, without the file ending, which, in this case,
+# is invariably '.c'.
+
+# Go through each directory, search for a Makefile and parse it to find "tests". Print the results.
+
+def process_line(line)
+	if(line.beginsWith('tests'))
+		#p line
+		words = line.scan(/[^ \t]+/)
+		return nil unless(words[0] == 'tests' || words[0] == 'tests-static')
+		#p words
+		if(words[1] == '=' || words[1] == ':=' || words[1] == '+=')
+			return words.slice(2..-1)
+		end
+	end
+	return nil
+end
+
+SPACE_MARK = "__&NBSP;__"
+def parse_makefile(filename)
+	tests = []
+	open(filename) do |mf|
+		# Read the entire file.
+		lines = mf.read
+		
+		# Get rid of comments, and combine logical lines into physical lines, for easy processing.
+		lines.gsub!(/\\ /, SPACE_MARK)
+		lines.gsub!(/#[^\n]*\n/m, "")
+		lines.gsub!(/\\\n/, ' ')
+		lines.split("\n").each do |line|
+			new_tests = process_line(line)
+			#p new_tests
+			tests += new_tests if(new_tests)
+		end
+	end
+	return tests
+end
+
+pattern = "#{SETTINGS[:source_path]}*/"
+p pattern
+dirs = Dir[pattern]
+total = 0
+files = []
+#p dirs
+dirs.each do |dir|
+	dirName = File.basename(dir)
+	if(SKIPPED_DIRECTORIES.include?(dirName))
+		puts "Skipped #{dirName}"
+		next
+	end
+	mfPath = dir + 'Makefile'
+	if(File.exists?(mfPath))
+		$stdout.write dirName + ': '
+		tests = parse_makefile(mfPath)
+		if(tests)
+			puts tests.size
+			total += tests.size
+			#p tests
+			tests.each do |t|
+				files << dir + t + '.c'
+			end
+		end
+	else
+		puts dirName + ' has no Makefile.'
+	end
+end
+
+puts
+puts "Total: #{total}"
+
+# compile all tests
+
+def delete_if_empty(filename)
+	if(!File.size?(filename))
+		if(File.exists?(filename))
+			FileUtils.rm(filename)
+		end
+	end
+end
+
+def link_and_test(ofn, dead_code, force_rebuild)
+	suffix = dead_code ? 'e' : ''
+	pfn = ofn.ext('.moo' + suffix)
+	winFile = ofn.ext('.win' + suffix)
+	failFile = ofn.ext('.fail' + suffix)
+	logFile = ofn.ext('.log' + suffix)
+	mdsFile = ofn.ext('.md.s')
+	esFile = ofn.ext('.e.s')
+	sldFile = ofn.ext('.sld')
+	
+	delete_if_empty(pfn)
+	
+	# link
+	if(!File.exists?(pfn) || force_rebuild)
+		sld_flags = " -sld=#{sldFile}"
+		if(dead_code)
+			sh "pipe-tool#{PIPE_FLAGS} -elim -master-dump -B #{pfn} #{ofn} #{PIPE_LIBS}"
+			sh "pipe-tool#{sld_flags} -B #{pfn} rebuild.s"
+		else
+			sh "pipe-tool#{sld_flags}#{PIPE_FLAGS} -B #{pfn} #{ofn} #{PIPE_LIBS}"
+		end
+		force_rebuild = true
+	end
+	delete_if_empty(pfn)
+	if(!File.exists?(pfn))
+		error"Unknown link failure."
+	end
+	
+	# execute it, if not win already, or we rebuilt something.
+	
+	if((File.exists?(winFile) || !SETTINGS[:retry_failed]) && !force_rebuild)
+		return force_rebuild
+	end
+	cmd = "#{MOSYNCDIR}/bin/more -noscreen -program #{pfn} -sld #{sldFile}"
+	$stderr.puts cmd
+	res = system(cmd)
+	puts res
+	if(res == true)	# success
+		FileUtils.touch(winFile)
+		FileUtils.rm_f(failFile)
+		FileUtils.rm_f(logFile)
+		FileUtils.rm_f(mdsFile)
+		FileUtils.rm_f(esFile)
+	else	# failure
+		FileUtils.touch(failFile)
+		FileUtils.rm_f(winFile)
+		FileUtils.mv('log.txt', logFile) if(File.exists?('log.txt'))
+		FileUtils.mv('_masterdump.s', mdsFile) if(File.exists?('_masterdump.s'))
+		FileUtils.mv('rebuild.s', esFile) if(File.exists?('rebuild.s'))
+		if(SETTINGS[:stop_on_fail])
+			error "Stop on fail"
+		end
+	end
+	return force_rebuild
+end
+
+#puts "premature exit"
+#exit 0
+
+files.each do |filename|
+	bn = File.basename(filename)
+	if(!File.exists?(filename))
+		puts "Nonexistant: #{bn}"
+		next
+	end
+	if(SKIPPED_FILES.include?(bn))
+		puts "Skipped #{bn}"
+		next
+	end
+	puts bn
+	
+	ofn = BUILD_DIR + '/' + bn.ext('.s')
+	force_rebuild |= SETTINGS[:rebuild_failed] && (File.exists?(ofn.ext('.fail')) || File.exists?(ofn.ext('.faile')))
+	
+	# compile
+	if(!File.exists?(ofn) || force_rebuild)
+		sh "#{MOSYNCDIR}/bin/xgcc -g -S \"#{filename}\" -o #{ofn}#{GCC_FLAGS} -I \"#{File.dirname(filename)}\""
+		force_rebuild = true
+	end
+	
+	force_rebuild = link_and_test(ofn, false, force_rebuild)
+	#link_and_test(ofn, true, force_rebuild)
+end

tests/libc-suite/error.h
@@ -1,0 +1,51 @@
+/* Declaration for error-reporting function
+   Copyright (C) 1995,1996,1997,2003,2006,2007 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#ifndef _ERROR_H
+#define _ERROR_H 1
+
+/* Print a message with `fprintf (stderr, FORMAT, ...)';
+   if ERRNUM is nonzero, follow it with ": " and strerror (ERRNUM).
+   If STATUS is nonzero, terminate the program with `exit (STATUS)'.  */
+
+extern void error (int __status, int __errnum, __const char *__format, ...)
+     __attribute__ ((__format__ (__printf__, 3, 4)));
+
+extern void error_at_line (int __status, int __errnum, __const char *__fname,
+			   unsigned int __lineno, __const char *__format, ...)
+     __attribute__ ((__format__ (__printf__, 5, 6)));
+
+/* If NULL, error will flush stdout, then print on stderr the program
+   name, a colon and a space.  Otherwise, error will call this
+   function without parameters instead.  */
+extern void (*error_print_progname) (void);
+
+/* This variable is incremented each time `error' is called.  */
+extern unsigned int error_message_count;
+
+/* Sometimes we want to have at most one error per line.  This
+   variable controls whether this mode is selected or not.  */
+extern int error_one_per_line;
+
+
+#if defined __extern_always_inline && defined __va_arg_pack
+# include <bits/error.h>
+#endif
+
+#endif /* error.h */

tests/libc-suite/features.h
@@ -1,0 +1,7 @@
+#ifdef __cplusplus
+#define __BEGIN_DECLS extern "C" {
+#define __END_DECLS }
+#else
+#define __BEGIN_DECLS
+#define __END_DECLS
+#endif

tests/libc-suite/fenv.h
@@ -1,0 +1,12 @@
+#define FE_ALL_EXCEPT 0
+#define FE_OVERFLOW 0
+#define FE_UNDERFLOW 0
+#define FE_INEXACT 0
+
+inline int feclearexcept(int excepts) {
+	return 0;
+}
+
+inline int fetestexcept (int excepts) {
+	return 0;
+}

tests/libc-suite/helpers.c
@@ -1,0 +1,69 @@
+#include <ma.h>
+#include "error.h"
+#include "mavsprintf.h"
+#include "maassert.h"
+#include <errno.h>
+
+int main();
+
+int MAMain() {
+	return main();
+}
+
+void error(int __status, int __errnum, __const char* __format, ...)
+{
+	va_list args;
+	va_start(args, __format);
+	char buffer[2048];
+	vsprintf(buffer, __format, args);
+	va_end(args);
+	lprintfln("error(%i, %i, %s)\n", buffer);
+	maExit(1);
+}
+
+int nanosleep(const struct timespec *requested_time, struct timespec *remaining) {
+	BIG_PHAT_ERROR;
+}
+
+int kill(int pid, int sig) {
+	BIG_PHAT_ERROR;
+	errno = ENOSYS;	// not implemented
+	return -1;
+}
+
+pid_t waitpid(pid_t pid, int* status_ptr, int options) {
+	BIG_PHAT_ERROR;
+	errno = ENOSYS;
+	return -1;
+}
+
+const char* strsignal(int __signo) {
+	BIG_PHAT_ERROR;
+	errno = ENOSYS;
+	return NULL;
+}
+
+int mcheck (void (*__abortfunc) (enum mcheck_status)) {
+	return 0;
+}
+int mcheck_pedantic (void (*__abortfunc) (enum mcheck_status)) {
+	return 0;
+}
+void mcheck_check_all (void) {
+}
+void mtrace (void) {
+}
+void muntrace (void) {
+}
+
+long int random (void) {
+	return rand();
+}
+
+double nexttoward(double x, double y) {
+	return nextafter(x, y);
+}
+
+float nexttowardf(float x, double y) {
+	return nextafterf(x, y);
+}

commit 160cd4475b50251fd22102015bb33f6b66c8009b

Trunk, newlib: Fixed a bug in open() that was introduced in r1644. testPrograms: Added newlib support to buildbase.bat. Added a very simple test case for open().
git-svn-id: svn://localhost/public/MoSync/trunk@1652 6ad7a17e-d552-4a1e-ace4-f958fe14b117

libs/newlib/libc/sys/mosync/machine.c
@@ -325,8 +325,10 @@ int open(const char * __filename, int __mode, ...) {
 			return -1;
 		}
 	}
-	sFda[newFd]->lowFd = handle + LOWFD_OFFSET;
-	sFda[newFd]->refCount = 1;
+	
+	newLfd->lowFd = handle + LOWFD_OFFSET;
+	newLfd->refCount = 1;
+	sFda[newFd] = newLfd;
 	return newFd;
 }
 

testPrograms/buildbase.bat
@@ -5,7 +5,18 @@ echo Compiling %1...
 REM  2>&1 | sed 's/\([-a-zA-Z0-9/]\+\)\(.[-a-zA-Z0-9]\+\):\([0-9]\+\):/%CD:\=\/%\/\1\2(\3):/'
 REM -Wshadow
 
-set IFLAGS=-I%MOSYNCDIR%/include -I%MOSYNCDIR%/libs -I%MOSYNCDIR%/profiles/vendors/MobileSorcery/Emulator
+IF E%NEWLIB%==EY GOTO USE_NEWLIB
+set INCLUDE_POSTFIX=
+set LIBDIR_PREFIX=pipe
+set STDLIB=mastd
+GOTO SET_FLAGS
+:USE_NEWLIB
+set INCLUDE_POSTFIX=/newlib
+set LIBDIR_PREFIX=newlib
+set STDLIB=newlib
+
+:SET_FLAGS
+set IFLAGS=-I%MOSYNCDIR%/include%INCLUDE_POSTFIX% -I%MOSYNCDIR%/libs -I%MOSYNCDIR%/profiles/vendors/MobileSorcery/Emulator
 set CFLAGS=-finput-charset=UTF-8 -O0 -fno-omit-frame-pointer -Wall -Werror -Wextra -Wno-unused-parameter -Wwrite-strings
 echo xgcc -g -S %IFLAGS% %CFLAGS% -o temp.s %1
 xgcc -g -S %IFLAGS% %CFLAGS% -o temp.s %1
@@ -17,18 +28,18 @@ echo Done.
 
 IF E%ELIM%==Ejava GOTO JAVA
 IF NOT E%ELIM%==EY GOTO REGULAR
-pipe-tool -xerr -elim -B program_full temp.s "%MOSYNCDIR%\lib\pipe_release\mastd.lib" "%MOSYNCDIR%\lib\pipe_release\mautil.lib"
+pipe-tool -xerr -elim -B program_full temp.s "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_release\%STDLIB%.lib" "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_release\mautil.lib"
 pipe-tool -xerr -B program rebuild.s
 GOTO COPYFILES
 
 :JAVA
-@REM pipe-tool -xerr -elim -B program_full temp.s "%MOSYNCDIR%\lib\pipe_debug\mastd.lib" "%MOSYNCDIR%\lib\pipe_debug\mautil.lib"
-pipe-tool -java "-gcj=\"--CLASSPATH=c:/Program/Research In Motion/BlackBerry JDE 5.0.0/lib/net_rim_api.jar;c:/SonyEricsson/JavaME_SDK_CLDC/PC_Emulation/WTK2/apps/JavaVM/classes/\"" -master-dump -B program temp.s "%MOSYNCDIR%\lib\pipe\mastdD.lib" "%MOSYNCDIR%\lib\pipe\mautilD.lib"
+@REM pipe-tool -xerr -elim -B program_full temp.s "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_debug\%STDLIB%.lib" "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_debug\mautil.lib"
+pipe-tool -java "-gcj=\"--CLASSPATH=c:/Program/Research In Motion/BlackBerry JDE 5.0.0/lib/net_rim_api.jar;c:/SonyEricsson/JavaME_SDK_CLDC/PC_Emulation/WTK2/apps/JavaVM/classes/\"" -master-dump -B program temp.s "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_debug\%STDLIB%.lib" "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_debug\mautil.lib"
 call prever.bat
 GOTO end
 
 :REGULAR
-set CM=pipe-tool -stacksize=18000 -datasize=128000 -master-dump -sld=sld.tab -stabs=stabs.tab -no-verify -B program temp.s "%MOSYNCDIR%\lib\pipe_debug\mastd.lib" "%MOSYNCDIR%\lib\pipe_debug\mautil.lib" "%MOSYNCDIR%\lib\pipe_debug\maui.lib" "%MOSYNCDIR%\lib\pipe_debug\mtxml.lib"
+set CM=pipe-tool -stacksize=64000 -datasize=512000 -master-dump -sld=sld.tab -stabs=stabs.tab -no-verify -B program temp.s "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_debug\%STDLIB%.lib" "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_debug\mautil.lib" "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_debug\maui.lib" "%MOSYNCDIR%\lib\%LIBDIR_PREFIX%_debug\mtxml.lib"
 echo %CM%
 %CM%
 

testPrograms/newlib-open/main.cpp
@@ -1,0 +1,18 @@
+
+#include <ma.h>
+#include <maassert.h>
+#include <conprint.h>
+#include <sys/fcntl.h>
+#include <sys/unistd.h>
+#include <errno.h>
+
+int rc;
+const char *dbname = "/my_db.sql3";
+
+int MAMain() {
+	rc = open( dbname, O_WRONLY | O_CREAT);
+	fprintf(stderr, "rc= %-3d %i %s \n", rc, errno, dbname);
+	close(rc);
+	FREEZE;
+	return 0;
+}