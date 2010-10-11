38188fe87dc80d64bbefe1f2ffa4a7bc1970554d
micke <micke@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-10-11 12:28:41 +0000

    trunk: Reverted changes committed by mistake.
    git-svn-id: svn://localhost/public/MoSync/trunk@2133 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- examples/Moblet/btServer/server.cpp
+++ examples/Moblet/btServer/server.cpp
@@ -30,93 +30,61 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 
 using namespace MAUtil;
 
-class MyConnectionHandler : public ConnectionListener
-{
+class MyConnectionHandler : public ConnectionListener {
 private:
 	Connection* mConn;
 	char mBuffer[64];
-	char mOutBuffer[64];
 	const int mId;
 public:
-	MyConnectionHandler(int id) : mConn(NULL), mId(id)
-	{
-	}
+	MyConnectionHandler(int id) : mConn(NULL), mId(id) {}
 
-	void start(Connection* conn)
-	{
+	void start(Connection* conn) {
 		mConn = conn;
 
 		// Print the remote address.
 		MAConnAddr addr;
 		int res = mConn->getAddr(&addr);
-		//MAASSERT(res > 0);
-		if (res <= 0)
-		{
-			printf("ERROR start: res <= 0");
-			return;
-		}
-
+		MAASSERT(res > 0);
 		byte* a = addr.bt.addr.a;
 		printf("%i @ %02X%02X%02X%02X%02X%02X\n", mId,
 			a[0], a[1], a[2], a[3], a[4], a[5]);
 
 		// Start receiving data.
 		mConn->setListener(this);
-		mConn->recv(mBuffer, 63);
+		mConn->recv(mBuffer, 4);
 	}
 
-	virtual ~MyConnectionHandler()
-	{
+	virtual ~MyConnectionHandler() {
 		delete mConn;
 	}
 
 	//ConnectionListener
-	void connRecvFinished(Connection* conn, int result)
-	{
-		if (result < 0)
-		{
-			// Error.
-			printf("ERROR recv mId: %i: result: %i\n", mId, result);
-			delete this; // should be safe
-		}
-		else
-		{
+	void connRecvFinished(Connection* conn, int result) {
+		if(result < 0) {	// error
+			printf("recv %i: %i\n", mId, result);
+			delete this;	// should be safe
+		} else {
 			// We received data. Print it.
 			mBuffer[result] = 0;
-			printf("Received data id: %i: buffer: %s\n", mId, mBuffer);
-
-			// Echo data back to the client.
-			mOutBuffer[0] = 70;
-			conn->write(mOutBuffer, 1);
-			printf("Writing data");
-
+			printf("%i: %s\n", mId, mBuffer);
 			// Continue receiving data.
 			conn->recv(mBuffer, 63);
-			printf("Receiving data");
 		}
-	}
-
-	void connWriteFinished(Connection* conn, int result)
-	{
-		printf("connWriteFinished result: %i", result);
 	}
 };
 
-class MyMoblet : public Moblet, public ServerListener
-{
+class MyMoblet : public Moblet, public ServerListener {
 private:
 	Server mServer;
 	int mNextId;
-	MyConnectionHandler* mConnectionHandler;
+	MyConnectionHandler* mCH;
 public:
-	MyMoblet() : mServer(this), mNextId(1)
-	{
+	MyMoblet() : mServer(this), mNextId(1) {
 		// Start the server.
 		printf("Hello World!\n");
-		//maPanic(1, "Iiiiiiiiiiiiiiiiiiiiii");
 		int res = mServer.start("btspp://localhost:"SERVER_UUID_STRING";name=btServer");
 		printf("startRes: %i\n", res);
-		if (res < 0)
+		if(res < 0)
 			return;
 
 		// Print the server's address.
@@ -128,51 +96,34 @@ public:
 		printf("@ %i %02X%02X%02X%02X%02X%02X\n", addr.bt.port,
 			a[0], a[1], a[2], a[3], a[4], a[5]);
 
-		printf("accept 1");
 		accept();
 	}
 
-	void accept()
-	{
+	void accept() {
 		// Create a handler for the new connection.
 		// Give it an id, so we can tell different connections apart.
 		// No need to save the pointer; the handler will delete itself
 		// when the connection closes.
-		mConnectionHandler = new MyConnectionHandler(mNextId++);
-		mServer.accept(mConnectionHandler);
+		mCH = new MyConnectionHandler(mNextId++);
+		mServer.accept(mCH);
 	}
 
-	void keyPressEvent(int keyCode, int nativeCode)
-	{
-		if (keyCode == MAK_0)
+	void keyPressEvent(int keyCode, int nativeCode) {
+		if(keyCode == MAK_0)
 			close();
 	}
 
-	void pointerPressEvent(MAPoint2d point)
-	{
-		printf("Closing server app");
-		close();
-	}
-
 	//ServerListener
-	void serverAcceptFailed(Server* server, int result)
-	{
+	void serverAcceptFailed(Server* server, int result) {
 		printf("accept failed: %i\n", result);
 	}
-
-	void serverAccepted(Server*, Connection* newConnection)
-	{
-		mConnectionHandler->start(newConnection);
-		printf("accept 2");
+	void serverAccepted(Server*, Connection* newConnection) {
+		mCH->start(newConnection);
 		accept();
 	}
 };
 
-#if !BUILD_CLIENT
-extern "C" int MAMain()
-{
+extern "C" int MAMain() {
 	Moblet::run(new MyMoblet());
 	return 0;
 };
-#endif
-
