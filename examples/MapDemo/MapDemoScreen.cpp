244e1b976a033bfd48f78b93b04b9d37bae33153
romain <romain@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-10-22 13:13:02 +0000

    Trunk, Examples: Commented out CloudMade maps list items from the default list due to API key requirement.
    git-svn-id: svn://localhost/public/MoSync/trunk@2222 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- examples/MapDemo/MapDemoScreen.cpp
+++ examples/MapDemo/MapDemoScreen.cpp
@@ -189,8 +189,13 @@ namespace MapDemo
 		list.add( newobject( MapSourceAction, new MapSourceAction( mMap, mMapSourceKind, MapSourceKind_GoogleMap, "Google (map)" ) ) );
 		list.add( newobject( MapSourceAction, new MapSourceAction( mMap, mMapSourceKind, MapSourceKind_GoogleAerial, "Google (aerial)" ) ) );
 		list.add( newobject( MapSourceAction, new MapSourceAction( mMap, mMapSourceKind, MapSourceKind_GoogleHybrid, "Google (hybrid)" ) ) );
-		list.add( newobject( MapSourceAction, new MapSourceAction( mMap, mMapSourceKind, MapSourceKind_CloudMade1, "CloudMade (type 1)" ) ) );
-		list.add( newobject( MapSourceAction, new MapSourceAction( mMap, mMapSourceKind, MapSourceKind_CloudMade7, "CloudMade (type 7)" ) ) );
+		//
+		// Uncomment the following lines if you want to use CloudMade maps with your API key.
+		// Without any key CloudMade maps will not be loaded.
+		// See MapDemoMoblet.cpp for more information about the API Key.
+		//
+		//list.add( newobject( MapSourceAction, new MapSourceAction( mMap, mMapSourceKind, MapSourceKind_CloudMade1, "CloudMade (type 1)" ) ) );
+		//list.add( newobject( MapSourceAction, new MapSourceAction( mMap, mMapSourceKind, MapSourceKind_CloudMade7, "CloudMade (type 7)" ) ) );
 		list.add( newobject( ExitAction, new ExitAction( mMoblet ) ) );
 	}
 }
