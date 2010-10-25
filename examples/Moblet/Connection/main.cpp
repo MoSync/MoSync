6be919942c7538031abe06dbdbf9a30ce28bf693
anders <anders@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-10-25 14:58:43 +0000

    trunk: Issue 886, fixed the Connection example and added some comments and information for the user.
    git-svn-id: svn://localhost/public/MoSync/trunk@2240 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- examples/Moblet/Connection/main.cpp
+++ examples/Moblet/Connection/main.cpp
@@ -15,6 +15,14 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 02111-1307, USA.
 */
 
+/**
+* This example shows how to do basic Moblet based HTTP/HTTPS communication.
+* 
+* If content-length is returned as an response header, then the first 1024
+* bytes of the content will be read.
+*
+*/
+
 #include <maapi.h>
 #include <MAUtil/Moblet.h>
 #include <MAUtil/Connection.h>
@@ -26,16 +34,44 @@ using namespace MAUtil;
 
 #define BUFSIZE 1024
 #define EXAMPLE_URL "http://www.example.com/"
-#define SECURE_URL "https://secure.wikimedia.org/"
+#define SECURE_URL "https://encrypted.google.com/"
 
+/**
+* Moblet class.
+* HttpConnectionListener inherites the http listener functions.
+*/
 class MyMoblet : public Moblet, private HttpConnectionListener {
 public:
+
+	/**
+	* Constructor
+	* mHttp(this) activates our http listeners.
+	*/
 	MyMoblet() : mHttp(this) {
-		start(SECURE_URL);
+		showInformation();
 	}
 	
+	/**
+	* Shows basic information on how to use the program
+	*/
+	void showInformation()
+	{
+		printf("HTTP connection -\n");
+		printf("   Press fire key or\n");
+		printf("   touch the screen\n");
+		printf("HTTPS connection -\n");
+		printf("   Press 1 key or\n");
+		printf("   soft left key\n");
+		printf("To EXIT -\n");
+		printf("   Press 0 key or\n");
+		printf("   soft right key\n");
+	}
+
+	/**
+	* Initiates and establishes a new connection with the given url.
+	*/
 	void start(const char* url) {
-		printf("Connecting...\n");
+		printf("\nConnecting to,\n%s...\n", url);
 
 		mHttp.close();
 		int res = mHttp.create(url, HTTP_GET);
@@ -46,6 +82,12 @@ public:
 		}
 	}
 
+	/**
+	* Listener that is fired when a connection has been established.
+	* The function tries to read the content-length from the
+	* resoonse headers. If successfull, the first 1024 bytes
+	* will be read.
+	*/
 	virtual void httpFinished(HttpConnection* http, int result) {
 		printf("HTTP %i\n", result);
 		if(result <= 0) {
@@ -53,10 +95,9 @@ public:
 		}
 		String cl;
 		int res = mHttp.getResponseHeader("content-length", &cl);
-		printf("cl code %i\n", res);
+
 		if(res > 0) {
-			printf("cl strlen %i\n", cl.length());
-			printf("cl: %s\n", cl.c_str());
+			printf("content-length: %s bytes\n", cl.c_str());
 			int len = atoi(cl.c_str());
 			if(len >= BUFSIZE) {
 				printf("Buffer too small, can't read everything.\n");
@@ -65,11 +106,25 @@ public:
 			mBuffer[len] = 0;
 			mHttp.read(mBuffer, len);
 		}
-	}
-	virtual void connReadFinished(Connection* conn, int result) {
-		printf("connReadFinished %i\n", result);
+		else
+		{
+			printf("No content-length\n\n\n");
+			showInformation();
+		}
+
 	}
 
+	/**
+	* Listener that is fired when the content has been read.
+	*/
+	virtual void connReadFinished(Connection* conn, int result) {
+		printf("connReadFinished result: %i\n\n", result);
+		showInformation();
+	}
+
+	/**
+	* Moblet listener that is fired when a key has been pressed.
+	*/
 	void keyPressEvent(int keyCode, int nativeCode) {
 		if(keyCode == MAK_0 || keyCode == MAK_SOFTRIGHT)
 			maExit(0);
@@ -79,6 +134,9 @@ public:
 			start(SECURE_URL);
 	}
 	
+	/**
+	* Moblet listener that has been fired when the screen has been pressed.
+	*/
 	void pointerPressEvent(MAPoint2d p) {
 		start(EXAMPLE_URL);
 	}
@@ -88,10 +146,20 @@ public:
 	}
 
 private:
+	/**
+	* The char buffer that is used for storing the content.
+	*/
 	char mBuffer[BUFSIZE];
+
+	/**
+	* The connection object used for the connection.
+	*/
 	HttpConnection mHttp;
 };
 
+/**
+* Program entry point which initiates the console and starts the Moblet.
+*/
 extern "C" int MAMain() {
 	InitConsole();
 	gConsoleLogging = 1;
