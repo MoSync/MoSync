e49e89ce192756c75ba8781cd77996d3569dde10
fmattias <fmattias@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-11-23 16:43:07 +0000

    trunk, winmobile: Fixed issue 963 and refactored the vibration code (tested on WM SP2003, 5.0, 6.0, 6.1, 6.5).
    git-svn-id: svn://localhost/public/MoSync/trunk@2396 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- runtimes/cpp/platforms/winmobile/Vibration.h
+++ runtimes/cpp/platforms/winmobile/Vibration.h
@@ -18,12 +18,82 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #ifndef _VIBRATION_H_
 #define _VIBRATION_H_
 
-namespace Base {
+/**
+ * On some windows platforms the NLed.h header is not included, but the
+ * actual functions are available. Therefore the contents relevant to the 
+ * vibration functionality is included below.
+ */
+#ifndef NLED_COUNT_INFO_ID
+extern "C"
+{
+#define NLED_COUNT_INFO_ID 0
+#define NLED_SUPPORTS_INFO_ID 1
+#define NLED_SETTINGS_INFO_ID 2
 
-// true on success, false otherwise
-int VibrationStart(int ms);
-int VibrationStop();
+	struct NLED_COUNT_INFO
+	{
+		UINT cLeds;
+	};
 
+	struct NLED_SUPPORTS_INFO
+	{
+		UINT LedNum;
+		LONG lCycleAdjust;
+		BOOL fAdjustTotalCycleTime;
+		BOOL fAdjustOnTime;
+		BOOL fAdjustOffTime;
+		BOOL fMetaCycleOn;
+		BOOL fMetaCycleOff;
+	};
+
+	struct NLED_SETTINGS_INFO
+	{
+		UINT LedNum;
+		INT OffOnBlink;
+		LONG TotalCycleTime;
+		LONG OnTime;
+		LONG OffTime;
+		INT MetaCycleOn;
+		INT MetaCycleOff;
+	};
+
+	BOOL NLedGetDeviceInfo(INT nID, PVOID pOutput);
+	BOOL NLedSetDevice(INT nID, PVOID pOutput);
+}
+#endif
+
+namespace Base
+{
+	/**
+	 * This namespace handles the vibration of a phone. Since the vibration
+	 * functionality is stateless this is a namespace rather than a class.
+	 */
+	namespace Vibration
+	{
+		/**
+		 * Determines whether the given device supports the vibrator LED.
+		 *
+		 * @return true if the device supports the vibrator LED, false
+		 *         otherwise.
+		 */
+		bool available();
+
+		/**
+		 * Turns on the vibrator for the given number of miliseconds, and
+		 * then turns it off. If a vibrator is not available this function
+		 * will just return.
+		 *
+		 * @param vibratorOnTimeMs The number of miliseconds that the vibrator
+		 *                         will be turned on.
+		 * @return true if the vibration is started, false otherwise.
+		 */
+		bool onFor(int vibratorOnTimeMs);
+
+		/**
+		 * Stops the vibrator regardless whether it is on or off.
+		 */
+		void stop();
+	}
 }
 
 #endif /* _VIBRATION_H_ */
