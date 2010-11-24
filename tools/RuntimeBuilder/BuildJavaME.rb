f79f4e5629084e490f981532be6f5be31830ebb1
ali <ali@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-11-24 21:16:29 +0000

    Trunk: An overhaul on the j2me runtime builder. The overhaul was made for two reasons, the first reason was to be able to build multiple runtimes concurrently. This was not previously possibly because of the fact that it used global files, but also because the preverify program created a temporary directory based on the UTC time, which leaves a lot of space for name collisions. The second reason for the overhaul is that you can now also build on Linux, which will speed up the building of a single runtime considerably.
    git-svn-id: svn://localhost/public/MoSync/trunk@2402 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- tools/RuntimeBuilder/BuildJavaME.rb
+++ tools/RuntimeBuilder/BuildJavaME.rb
@@ -13,7 +13,7 @@
 # along with this program; see the file COPYING.  If not, write to the Free
 # Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
-
+require 'pathname'
 require 'fileutils'
 
 class RuntimeBuilder 
@@ -30,17 +30,23 @@ class RuntimeBuilder
 		java_file = src_file.gsub(/.jpp$/, ".java")
 		
 		puts "Processing " + java_file
-		
+
+		#
 		# Preprocess the jpp file into a jtmp file, sed fixes the output if any
-		sh("xgcc -x c -E -o #{output_dir}#{jtmp_file} -D#{platform_define} -I#{$SETTINGS[:java_source]}shared -I#{platform_dir}" +
+		# Note: The include order is very important, infact, it is critical 
+		#       that output_dir is included before any of the other include 
+		#       dirs. config_platform.h is located in output dir. That one 
+		#       should override any other.
+		#
+		sh("xgcc -x c -E -o #{output_dir}/#{jtmp_file} -D#{platform_define} -I#{output_dir} -I#{$SETTINGS[:java_source]}/Shared -I#{platform_dir}" +
 		       " #{src_dir}#{src_file} 2>&1 | sed \"s/\\([a-zA-Z/]\\+\\)\\(.[a-zA-Z]\\+\\):\\([0-9]\\+\\):/\\1\\2(\\3):/\"")
 		
 		# Use sed to comment the lines which the proprocessor added to the file and save it as a java file
-		sh("sed \"s/^# /\\/\\//\" < #{output_dir}#{jtmp_file} > #{output_dir}#{java_file}");
+		sh("sed \"s/^# /\\/\\//\" < #{output_dir}/#{jtmp_file} > #{output_dir}/#{java_file}");
 	end
 
 	def preprocess_shared_java_files(output_dir, platform_dir, platform_define)
-		shared_src = "#{$SETTINGS[:java_source]}shared/";
+		shared_src = "#{$SETTINGS[:java_source]}/Shared/";
 
 		Dir.foreach(shared_src) {|x| 
 			if (x == "BigPhatError.jpp" || x == "Binary.jpp" || x == "BinaryInterface.jpp" || x == "Core.jpp" ||
@@ -51,35 +57,42 @@ class RuntimeBuilder
 		}
 	end
 	
-	def javameBuilder(runtime_dir, mode, cldc10)
+	def javameBuilder(runtime_dir, mode, cldc10)		
+		#
+		# Setup
+		#
 		debug = (mode=="debug") ? "D" : ""
+		$SETTINGS[:java_source] = Pathname::new( $SETTINGS[:java_source] ).realpath.to_s
+		runtime_dir = Pathname::new( runtime_dir ).realpath.to_s
 		
-		java_me_sdk = "#{$SETTINGS[:javame_sdk]}"
+		java_me_sdk = Pathname::new( "#{$SETTINGS[:javame_sdk]}" ).realpath.to_s
 		
 		if ENV['JAVAMESDKDIR'] != nil
 			java_me_sdk = ENV['JAVAMESDKDIR']
 		end
 		
-		java_me_source = "#{$SETTINGS[:java_source]}platforms/JavaME/src"
+		java_me_source = Pathname::new( "#{$SETTINGS[:java_source]}/platforms/javaME/src" ).realpath.to_s
 		
 		# Set up temporary dir
-		temp_dir = "#{runtime_dir}temp/"
+		temp_dir = "#{runtime_dir}/temp/"
 		if File.exist? temp_dir
 			FileUtils.rm_rf temp_dir # delete everything in it and itself
 		end
 		Dir.mkdir(temp_dir); # No such directory/file.. create a temp directory
+		temp_dir = Pathname::new( temp_dir ).realpath.to_s
 		
 		# Set up class dir
-		class_dir = "#{runtime_dir}class/"
+		class_dir = "#{runtime_dir}/class/"
 		if File.exist? class_dir
 			FileUtils.rm_rf class_dir # delete everything in it and itself
 		end
 		Dir.mkdir class_dir; # No such directory/file.. create a temp directory
+		class_dir = Pathname::new( class_dir ).realpath.to_s
 		
-		# Copy the old config_platform.h file and copy the one from the runtime_dir to the source location
-		config_file = "#{java_me_source}/config_platform.h"		
-		backup_file config_file
-		FileUtils.copy_file( "#{runtime_dir}config#{debug}.h", config_file)
+		# Copy the config file to the temp dir where all the 
+		# files will be pre-proccessed
+		config_file = "#{temp_dir}/config_platform.h"		
+		FileUtils.copy_file( "#{runtime_dir}/config#{debug}.h", config_file)
 		
 		# Preprocess all the shared java files and store result in temporary location
 		preprocess_shared_java_files(temp_dir, java_me_source, "_JavaME");
@@ -92,23 +105,19 @@ class RuntimeBuilder
 			end
 		}
 		
-		# Restore config_platform.h
-		revert_backupped_file config_file
-		
-		cldcjar = cldc10 ? 'cldcapi10' : 'cldcapi11'
-		
+		cldcjar = cldc10 ? 'cldcapi10' : 'cldcapi11'		
 		libjars = [cldcjar, 'midpapi20', 'jsr082', 'jsr179', 'wma20', 'mmapi', 'jsr75']
 		
 		# Compile Java source
-		puts "Compiling java source.."
+		puts "\nCompiling java source.."
 		sh("javac -source 1.4 -target 1.4 -d #{class_dir} -classpath #{class_dir} -bootclasspath " +
-			libjars.collect { |jar| "#{java_me_sdk}j2melib/#{jar}.jar;" }.join('') +
-			" #{temp_dir}*.java")
+			libjars.collect { |jar| "#{java_me_sdk}/j2melib/#{jar}.jar#{File::PATH_SEPARATOR}" }.join('') +
+			" #{temp_dir}/*.java")
 		
 		# Generate Manifest file
-		puts "Generating manifest file.."
+		puts "\nGenerating manifest file.."		
 		runtime_number = runtime_dir.split('/')[-1] # extract which runtime number it is from the path
-		File.open("#{temp_dir}manifest.mf", "w")  do |infile|
+		File.open("#{temp_dir}/manifest.mf", "w")  do |infile|
 			infile.puts "MIDlet-1: MoSyncRuntime#{runtime_number}, MoSyncRuntime#{runtime_number}.png, MAMidlet\n"
 			infile.puts "MIDlet-Name: MAMidlet\n"
 			infile.puts "MIDlet-Vendor: Mobile Sorcery\n"
@@ -118,42 +127,50 @@ class RuntimeBuilder
 		end
 		
 		# Build jar file
-		puts "Building jar file.."
-		dos_class_dir = class_dir.gsub(/\//, "\\") # fixes the slashes so that the jar file gets it right
-		sh("jar cfm #{runtime_dir}MoSyncRuntimeTemp.jar #{temp_dir}manifest.mf -C #{dos_class_dir} .")
+		puts "\nBuilding jar file.."
+		sh("jar cfm #{runtime_dir}/MoSyncRuntimeTemp.jar #{temp_dir}/manifest.mf -C #{class_dir} .")
 		
-		if !File.exist? "#{runtime_dir}MoSyncRuntimeTemp.jar"
+		if !File.exist? "#{runtime_dir}/MoSyncRuntimeTemp.jar"
 			puts "\nFATAL ERROR! - Unable to build jar file, check previous output for errors!\n\n"
 			return
 		end
 		
-		
 		# Obfuscate java binaries
-		puts "Obfuscating java binaries.."
-		sh("java -jar #{java_me_sdk}bin/proguard.jar -injars #{runtime_dir}MoSyncRuntimeTemp.jar " +
-			libjars.collect { |jar| "-libraryjars #{java_me_sdk}j2melib/#{jar}.jar " }.join('') +
+		puts "\nObfuscating java binaries.."
+		sh("java -jar #{java_me_sdk}/bin/proguard.jar -injars #{runtime_dir}/MoSyncRuntimeTemp.jar " +
+			libjars.collect { |jar| "-libraryjars #{java_me_sdk}/j2melib/#{jar}.jar " }.join('') +
 			"-dontusemixedcaseclassnames " +
-			"-outjars #{runtime_dir}MoSyncRuntimeObfuscated.jar -keep public class MAMidlet");
+			"-outjars #{runtime_dir}/MoSyncRuntimeObfuscated.jar -keep public class MAMidlet");
 		
-		FileUtils.copy_file("#{runtime_dir}MoSyncRuntimeObfuscated.jar", "#{runtime_dir}MoSyncRuntime#{debug}.jar" )
+		FileUtils.copy_file("#{runtime_dir}/MoSyncRuntimeObfuscated.jar", "#{runtime_dir}/MoSyncRuntime#{debug}.jar" )
 		
+		#
 		# Preverify java binaries
-		puts "Preverifying java binaries.."
-		sh("#{java_me_sdk}bin/preverify -d #{runtime_dir[0..-2]} -classpath " +
-			libjars.collect { |jar| "#{java_me_sdk}j2melib/#{jar}.jar;" }.join('') +
-			" #{runtime_dir}MoSyncRuntime#{debug}.jar")
+		# Note: It is very important that this operation is run from a 
+		#       temporary directory if we are to run several builds 
+		#       concurrently. The reason for this is that preverify 
+		#       creates a temporary directory based on the current UTC
+		#       time.
+		#
+		puts "\nPreverifying java binaries.."
+		curr_dir = FileUtils.pwd()
+		FileUtils.cd temp_dir		
+		sh("#{java_me_sdk}/bin/preverify -d #{runtime_dir} -classpath " +
+			libjars.collect { |jar| "#{java_me_sdk}/j2melib/#{jar}.jar#{File::PATH_SEPARATOR}" }.join('') +
+			" #{runtime_dir}/MoSyncRuntime#{debug}.jar")
+		FileUtils.cd curr_dir
 		
 		# Clean and delete all the temporary folders
 		FileUtils.rm_rf temp_dir
 		FileUtils.rm_rf class_dir
-		FileUtils.rm runtime_dir + "MoSyncRuntimeObfuscated.jar"
-		FileUtils.rm runtime_dir + "MoSyncRuntimeTemp.jar"
+		FileUtils.rm runtime_dir + "/MoSyncRuntimeObfuscated.jar"
+		FileUtils.rm runtime_dir + "/MoSyncRuntimeTemp.jar"
 		
-		if !File.exist? "#{runtime_dir}MoSyncRuntime#{debug}.jar"
+		if !File.exist? "#{runtime_dir}/MoSyncRuntime#{debug}.jar"
 			puts "\nFATAL ERROR! - No jar file built, check previous output for errors!\n\n"
 			return 1
 		else
-			puts "\nFINISHED! - #{runtime_dir}MoSyncRuntime#{debug}.jar was succesfully generated!\n\n"
+			puts "\nFINISHED! - #{runtime_dir}/MoSyncRuntime#{debug}.jar was succesfully generated!\n\n"
 			return 0
 		end
 		return 0
