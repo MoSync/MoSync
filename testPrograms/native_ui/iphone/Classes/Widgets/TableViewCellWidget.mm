2ef912857bbd1f5beaad3641984744bbee1d86f9
Niklas Nummelin <nummelin@Niklas-Nummelins-MacBook-Pro.local>
2010-12-06 16:01:32 +0100

    new-native-ui, iphone, moved widgets to new dir. Added accessoryType property to TableViewCell

--- testPrograms/native_ui/iphone/Classes/Widgets/TableViewCellWidget.mm
+++ testPrograms/native_ui/iphone/Classes/Widgets/TableViewCellWidget.mm
@@ -7,7 +7,7 @@
 //
 
 #import "TableViewCellWidget.h"
-
+#import "UIColor-Expanded.h"
 
 @implementation TableViewCellWidget
 
@@ -43,14 +43,30 @@
 - (int)setPropertyWithKey: (NSString*)key toValue: (NSString*)value {
 	if([key isEqualToString:@"text"]) {
 		UITableViewCell* cell = (UITableViewCell*) view;
-		cell.text = value;
-	} else 
-	if([key isEqualToString:@"backgroundColor"]) {
+		//cell.text = value;
+		UILabel* label = cell.textLabel;
+		label.text = value;
+	} 
+	else if([key isEqualToString:@"backgroundColor"]) {
 		//	[super addChild:child];
 		UITableViewCell *cell = (UITableViewCell*)view;
 		cell.contentView.backgroundColor = [UIColor colorWithHexString:value];
 		cell.textLabel.backgroundColor = [UIColor colorWithHexString:value];
-		
+		cell.accessoryView.backgroundColor = [UIColor colorWithHexString:value];		
+	} 
+    else if([key isEqualToString:@"accessoryType"]) {
+		UITableViewCell *cell = (UITableViewCell*)view;
+		if([value isEqualToString:@"hasChildren"]) {
+			cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+		} 
+		else if([value isEqualToString:@"hasDetails"]) {
+			cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+		}
+		else if([value isEqualToString:@"isChecked"]) {
+			cell.accessoryType = UITableViewCellAccessoryCheckmark;
+		} else {
+			cell.accessoryType = UITableViewCellAccessoryNone;			
+		}		   
 	} else {
 		return [super setPropertyWithKey:key toValue:value];
 	}
