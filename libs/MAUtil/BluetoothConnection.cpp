2f162391f1e7a5f377b716b6e44a08c876b8e4c7
Mikael Kindborg <mikael.kindborg@mosync.com>
2010-12-13 18:36:27 +0100

    New connect method for BluetoothConnection. Fixed bug in Bluetooth on Android.

--- libs/MAUtil/BluetoothConnection.cpp
+++ libs/MAUtil/BluetoothConnection.cpp
@@ -18,8 +18,13 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #include "BluetoothConnection.h"
 #include <maassert.h>
 #include <mavsprintf.h>
+#include <mastdlib.h>
 
 using namespace MAUtil;
+
+static byte hexsubstr2byte(const char* str, int start);
+static int hexsubstr2int(const char* str, int start);
+static int hex2int(const char* hex);
 
 // mPort: <0 when closed, ==0 when discovering, >0 when connect-ing/-ed.
 
@@ -30,11 +35,12 @@ BluetoothConnection::~BluetoothConnection() {
 	this->close();
 }
 
-int BluetoothConnection::connect(const MABtAddr& address, const MAUUID& uuid) {
+int BluetoothConnection::connect(
+	const MABtAddr& address, const MAUUID& uuid) {
 	ASSERT_MSG(mPort < 0, "Connection already active");
-	mAddr = address;
+	mAddress = address;
 	mPort = 0;
-	int res = mBD.startServiceDiscovery(address, uuid, this);
+	int res = mBluetoothDiscoverer.startServiceDiscovery(address, uuid, this);
 	if(res < 0) {
 		// No service discovery. Either we're on Android,
 		// or this device doesn't have bluetooth at all.
@@ -52,9 +58,42 @@ int BluetoothConnection::connect(const MABtAddr& address, const MAUUID& uuid) {
 	return res;
 }
 
+/**
+ * Connect to a Bluetooth URL that specifies a device address and a UUID.
+ * Example URL: "btspp://38E7D8C7605E:DCBDB690C17111DF851A0800200C9A66"
+ */
+int BluetoothConnection::connect(const char* url) {
+	MABtAddr address;
+	MAUUID uuid;
+
+	// Check that the url is a valid.
+	if (53 != strlen(url) ||
+		url != strstr(url, "btspp://") ||
+		':' != url[20])
+	{
+		return -1;
+	}
+
+	// Parse Bluetooth address values from url.
+	address.a[0] = hexsubstr2byte(url, 8);
+	address.a[1] = hexsubstr2byte(url, 10);
+	address.a[2] = hexsubstr2byte(url, 12);
+	address.a[3] = hexsubstr2byte(url, 14);
+	address.a[4] = hexsubstr2byte(url, 16);
+	address.a[5] = hexsubstr2byte(url, 18);
+
+	// Parse UUID values from url.
+	uuid.i[0] = hexsubstr2int(url, 21);
+	uuid.i[1] = hexsubstr2int(url, 29);
+	uuid.i[2] = hexsubstr2int(url, 37);
+	uuid.i[3] = hexsubstr2int(url, 45);
+
+	return connect(address, uuid);
+}
+
 void BluetoothConnection::close() {
 	mPort = -1;
-	mBD.cancel();
+	mBluetoothDiscoverer.cancel();
 	Connection::close();
 }
 
@@ -65,7 +104,7 @@ void BluetoothConnection::btNewService(const BtService& serv) {
 	mPort = serv.port;
 
 	char buf[64];
-	const byte* a = mAddr.a;
+	const byte* a = mAddress.a;
 	sprintf(buf, "btspp://%02x%02x%02x%02x%02x%02x:%i",
 		a[0], a[1], a[2], a[3], a[4], a[5], serv.port);
 	int res = Connection::connect(buf);
@@ -79,4 +118,47 @@ void BluetoothConnection::btServiceDiscoveryFinished(int state) {
 			state = CONNERR_NOTFOUND;
 		mListener->connectFinished(this, state);
 	}
+}
+
+/**
+ * Convert a two character substring of a string that contains a hex byte
+ * value to a byte.
+ * @param str The string that contains the hex value that we want.
+ * @param start The start index of the hex value.
+ * @return The byte value of the two hex digits that begin at start.
+ */
+static byte hexsubstr2byte(const char* str, int start)
+{
+	char hex[3];
+	hex[0] = str[start];
+	hex[1] = str[start + 1];
+	hex[2] = 0;
+	return (byte) hex2int(hex);
+}
+
+/**
+ * Convert a four character substring of a string that contains a hex byte
+ * value to an int.
+ * @param str The string that contains the hex value that we want.
+ * @param start The start index of the hex value.
+ * @return The int value of the four hex digits that begin at start.
+ */
+static int hexsubstr2int(const char* str, int start)
+{
+	char hex[9];
+	hex[0] = str[start];
+	hex[1] = str[start + 1];
+	hex[2] = str[start + 2];
+	hex[3] = str[start + 3];
+	hex[4] = str[start + 4];
+	hex[5] = str[start + 5];
+	hex[6] = str[start + 6];
+	hex[7] = str[start + 7];
+	hex[8] = 0;
+	return hex2int(hex);
+}
+
+static int hex2int(const char* hex)
+{
+	return (int) strtoul(hex, NULL, 16);
 }
