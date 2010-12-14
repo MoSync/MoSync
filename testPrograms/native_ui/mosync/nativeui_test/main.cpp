61bbdacac812c39a27f1ff26926917836e6a2242
Mattias Franberg <mattias.franberg@mosync.com>
2010-12-14 14:46:25 +0100

    Updated the native ui test program to include a list of items that can be clicked. Once clicked a webview is opened.

--- testPrograms/native_ui/mosync/nativeui_test/main.cpp
+++ testPrograms/native_ui/mosync/nativeui_test/main.cpp
@@ -1,14 +1,19 @@
 #include <maapi.h>
 
+#include <mavsprintf.h>
+
 #include <IX_WIDGET.h>
+
+#include "MAHeaders.h"
 
 const char * WIDGET_TYPE_BUTTON = "Button";
 const char * WIDGET_TYPE_LABEL = "Label";
-const char * WIDGET_TYPE_SCREEN = "Screen";
-const char * WIDGET_TYPE_LIST_VIEW = "ListView";
-const char * WIDGET_TYPE_LIST_VIEW_CELL = "ListViewCell";
+const char * WIDGET_TYPE_LIST = "ListView";
+const char * WIDGET_TYPE_LIST_ITEM = "ListViewItem";
 const char * WIDGET_TYPE_LAYOUT_VERTICAL = "VerticalLayout";
 const char * WIDGET_TYPE_LAYOUT_HORIZONTAL = "HorizontalLayout";
+const char * WIDGET_TYPE_SCREEN = "Screen";
+const char * WIDGET_TYPE_WEB_VIEW = "WebView";
 
 const char * WIDGET_PROPERTY_WIDTH = "width";
 const char * WIDGET_PROPERTY_HEIGHT = "height";
@@ -18,12 +23,13 @@ const char * WIDGET_PROPERTY_PADDING_TOP = "top";
 const char * WIDGET_PROPERTY_PADDING_RIGHT = "right";
 const char * WIDGET_PROPERTY_PADDING_BOTTOM = "bottom";
 
-const char * WIDGET_PROPERTY_HALIGNMENT = "halignment";
-const char * WIDGET_PROPERTY_VALIGNMENT = "valignment";
+const char * WIDGET_PROPERTY_HALIGNMENT = "horizontalAlignment";
+const char * WIDGET_PROPERTY_VALIGNMENT = "verticalAlignment";
 const char * WIDGET_PROPERTY_TEXT = "text";
 
 const char * WIDGET_PROPERTY_BACKGROUND_COLOR = "backgroundColor";
 const char * WIDGET_PROPERTY_FONT_COLOR = "fontColor";
+const char * WIDGET_PROPERTY_FONT_SIZE = "fontSize";
 
 const char * WIDGET_HORIZONTAL_LEFT = "left";
 const char * WIDGET_HORIZONTAL_CENTER = "center";
@@ -33,6 +39,13 @@ const char * WIDGET_VERTICAL_TOP = "top";
 const char * WIDGET_VERTICAL_CENTER = "center";
 const char * WIDGET_VERTICAL_BOTTOM = "bottom";
 
+int maWidgetSetPropertyInt(MAHandle handle, const char *property, int value)
+{
+	char buffer[256];
+	sprintf( buffer, "%d", value );
+	maWidgetSetProperty( handle, property, buffer );
+}
+
 /*
  * main.cpp
  *
@@ -41,58 +54,76 @@ const char * WIDGET_VERTICAL_BOTTOM = "bottom";
  */
 extern "C" int MAMain()
 {
+	int screenHeight = EXTENT_Y( maGetScrSize( ) );
+
+	int screen = maWidgetCreate( WIDGET_TYPE_SCREEN );
+
 	// Create root layout
 	int layout = maWidgetCreate( WIDGET_TYPE_LAYOUT_VERTICAL );
+	maWidgetSetPropertyInt( layout, WIDGET_PROPERTY_WIDTH, -1 );
+	maWidgetSetPropertyInt( layout, WIDGET_PROPERTY_HEIGHT, -1 );
 
 	// Create header with some properties.
 	int header = maWidgetCreate( WIDGET_TYPE_LABEL );
-	maWidgetSetProperty(header, WIDGET_PROPERTY_HEIGHT, "60" );
+	maWidgetSetPropertyInt(header, WIDGET_PROPERTY_HEIGHT, 60 );
+	maWidgetSetPropertyInt(header, WIDGET_PROPERTY_WIDTH, -1 );
 	maWidgetSetProperty(header, WIDGET_PROPERTY_TEXT, "Header" );
 	maWidgetSetProperty(header, WIDGET_PROPERTY_HALIGNMENT, "center" );
 	maWidgetSetProperty(header, WIDGET_PROPERTY_VALIGNMENT, "center" );
 	maWidgetSetProperty(header, WIDGET_PROPERTY_FONT_COLOR, "ffffff" );
+	maWidgetSetProperty(header, WIDGET_PROPERTY_FONT_SIZE, "20.0" );
 	maWidgetAddChild( layout , header );
 
 	// Create List
-	int list = maWidgetCreate( WIDGET_TYPE_LIST_VIEW );
+	int list = maWidgetCreate( WIDGET_TYPE_LIST );
+	maWidgetSetPropertyInt( list, WIDGET_PROPERTY_HEIGHT, screenHeight - 60 );
+	maWidgetSetPropertyInt( list, WIDGET_PROPERTY_WIDTH, -1 );
 	maWidgetAddChild( layout, list );
 
 	// Add 10 buttons to the list
 	for(int i = 0; i < 10; i++)
 	{
-		int listItem = maWidgetCreate( WIDGET_TYPE_BUTTON );
-		maWidgetSetProperty( listItem, WIDGET_PROPERTY_TEXT, "Click me" );
-		maWidgetSetProperty( listItem, WIDGET_PROPERTY_HEIGHT, "60" );
-		maWidgetSetProperty( listItem, WIDGET_PROPERTY_WIDTH, "100" );
+		int listItem = maWidgetCreate( WIDGET_TYPE_LIST_ITEM );
+		maWidgetSetProperty( listItem, "text", "Click me" );
+		maWidgetSetPropertyInt( listItem, "image", R_ICON );
 		maWidgetAddChild( list, listItem );
 	}
 
-	MAHandle mainScreen = maWidgetCreate(WIDGET_TYPE_SCREEN);
-
-	/*
-	int listItem = maWidgetCreate( WIDGET_TYPE_BUTTON );
-	maWidgetSetProperty( listItem, WIDGET_PROPERTY_TEXT, "Click me" );
-	maWidgetSetProperty( listItem, WIDGET_PROPERTY_HEIGHT, "60" );
-	maWidgetSetProperty( listItem, WIDGET_PROPERTY_WIDTH, "100" );
-	maWidgetAddChild( mainScreen, listItem );
-	*/
-
 	// Add the tree to the root
-	maWidgetAddChild( mainScreen, layout );
+	maWidgetAddChild( screen, layout );
+	maWidgetScreenShow( screen );
 
-	maWidgetScreenShow(mainScreen);
+	int webScreen = maWidgetCreate( WIDGET_TYPE_SCREEN );
+
+	int webView = maWidgetCreate( WIDGET_TYPE_WEB_VIEW );
+	maWidgetSetPropertyInt(webView, WIDGET_PROPERTY_WIDTH, -1 );
+	maWidgetSetPropertyInt(webView, WIDGET_PROPERTY_HEIGHT, -1 );
+
+	maWidgetAddChild( webScreen, webView );
 
 	// Wait for close event
 	while(1)
 	{
 		MAEvent event;
 
-		maWait(0);
+		maWait( 0 );
 		maGetEvent( &event );
 
 		if( event.type == EVENT_TYPE_FOCUS_LOST )
 		{
 			maExit( 0 );
+		}
+		else if( event.type == EVENT_TYPE_WIDGET )
+		{
+			MAWidgetEventData *eventData = (MAWidgetEventData *) event.data;
+
+
+			char buffer[100];
+			sprintf( buffer, "Last clicked %d\n", eventData->widgetHandle );
+			maWidgetSetProperty( header, WIDGET_PROPERTY_TEXT, buffer );
+
+			maWidgetSetProperty( webView, "url", "http://www.google.se/" );
+			maWidgetScreenShow( webScreen );
 		}
 	}
 }
