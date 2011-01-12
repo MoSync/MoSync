852634f526b7c78d7f9588f45adab07ea4801632
Magnus Hult <magnus@magnushult.se>
2011-01-12 14:54:59 +0100

    A set of multitouch events plus the iOS implementation of them.

--- runtimes/cpp/platforms/iphone/Classes/impl/Platform.h
+++ runtimes/cpp/platforms/iphone/Classes/impl/Platform.h
@@ -257,7 +257,7 @@ public:
 		pthread_mutex_unlock(&mMutex);	
 	}
 		
-	void addPointerEvent(int x, int y, int type) {
+	void addPointerEvent(int x, int y, int touchId, int type) {
 		if(!mEventOverflow) {
 			if(count() + 2 == EVENT_BUFFER_SIZE) {	//leave space for Close event
 				mEventOverflow = true;
@@ -269,6 +269,7 @@ public:
 			event.type = type;
 			event.point.x = x;
 			event.point.y = y;
+			event.touchId = touchId;
 			put(event);
 		}		
 	}
