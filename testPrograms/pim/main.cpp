4054ee8d70ced1ba80ac6a60f9f3f3de57bfee83
Fredrik Eldh <fredrik.eldh@mobilesorcery.com>
2011-01-12 15:29:37 +0100

    PIM writing: Wrote documentation. Implemented on JavaME. Added functions to testProgram.

--- testPrograms/pim/main.cpp
+++ testPrograms/pim/main.cpp
@@ -24,11 +24,14 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 
 #define DUMP_ITEMS 1
 #define USE_FILE 0
+#define ADD_REMOVE_ITEMS 1
 
 #if 1
 #define LOG printf
+#define CONSOLE_LOGGING 1
 #else
 #define LOG(...) lprintf(__VA_ARGS__)
+#define CONSOLE_LOGGING 0
 
 int lprintf(const char* fmt, ...) {
 	va_list args;
@@ -76,6 +79,7 @@ static void checkEvents() {
 static void listContacts() {
 	MAHandle list = maPimListOpen(MA_PIM_CONTACTS);
 	TEST(list);
+	printf("Contact list opened.\n");
 	while(1) {
 		MAHandle item = maPimListNext(list);
 		TEST(item);
@@ -106,7 +110,7 @@ static void dumpItem(MAHandle list, MAHandle item) {
 	TEST(item);
 	MA_PIM_ARGS args;
 	args.item = item;
-	char buf[128];
+	char buf[512];
 	args.buf = buf;
 	args.bufSize = sizeof(buf);
 	int nFields = maPimItemCount(item);
@@ -172,11 +176,139 @@ static void dumpItem(MAHandle list, MAHandle item) {
 }
 #endif
 
+#if ADD_REMOVE_ITEMS
+
+// return the number of bytes written
+template<int NUM>
+int writeStringArray(char* buf, const wchar_t* srca[NUM]) {
+	*(int*)buf = NUM;
+	wchar_t* dst = (wchar_t*)(buf + 4);
+	for(int i=0; i<NUM; i++) {
+		const wchar_t* src = srca[i];
+		while(true) {
+			*dst = *src;
+			dst++;
+			if(*src == 0)
+				break;
+			src++;
+		}
+	}
+	return (char*)dst - buf;
+}
+
+// add some dummy contacts
+// identify by single-value NOTE "tp_pim_dummy"
+static const wchar_t sDummyNote[] = L"tp_pim_dummy";
+static const wchar_t sDummyTelA[] = L"0123456789";
+static const wchar_t sDummyTelB[] = L"0987654321";
+static const wchar_t* sDummyName[] = { L"Smith", L"Agent", L"", L"", L"" };
+static const wchar_t sDummyEmail[] = L"foo@example.com";
+
+static void addContacts() {
+	char buf[512];
+	
+	printf("Adding dummy contact...\n");
+	// open & create
+	MAHandle list = maPimListOpen(MA_PIM_CONTACTS);
+	TEST(list);
+	MA_PIM_ARGS args;
+	TEST(args.item = maPimItemCreate(list));
+	
+	// add values
+	args.field = MA_PIM_FIELD_CONTACT_NOTE;
+	args.buf = (void*)sDummyNote;
+	args.bufSize = sizeof(sDummyNote);
+	TEST(maPimItemAddValue(&args, 0));
+	
+	args.field = MA_PIM_FIELD_CONTACT_TEL;
+	args.buf = (void*)sDummyTelA;
+	args.bufSize = sizeof(sDummyTelA);
+	TEST(maPimItemAddValue(&args, MA_PIM_ATTR_HOME));
+	
+	args.field = MA_PIM_FIELD_CONTACT_NAME;
+	args.buf = buf;
+	args.bufSize = writeStringArray<5>(buf, sDummyName);
+	TEST(maPimItemAddValue(&args, 0));
+	
+	// try SetValue
+	args.field = MA_PIM_FIELD_CONTACT_TEL;
+	args.buf = (void*)sDummyTelB;
+	args.bufSize = sizeof(sDummyTelB);
+	TEST(maPimItemSetValue(&args, 0, MA_PIM_ATTR_WORK | MA_PIM_ATTR_PREFERRED));
+	
+	// try RemoveValue
+	args.field = MA_PIM_FIELD_CONTACT_EMAIL;
+	args.buf = (void*)sDummyEmail;
+	args.bufSize = sizeof(sDummyEmail);
+	TEST(maPimItemAddValue(&args, 0));
+	
+	TEST(maPimItemRemoveValue(args.item, args.field, 0));
+	
+	// check maPimItemCount()
+	int count = maPimItemCount(args.item);
+	printf("Item count: %i\n", count);
+	if(count != 3) {
+		printf("BAD ITEM COUNT!\n");
+	}
+	
+	// close it up real good at the top
+	TEST(maPimItemClose(args.item));
+	TEST(maPimListClose(list));
+	printf("Done.\n");
+}
+
+// remove the dummy contacts added by addContacts
+static void removeContacts() {
+	printf("Removing dummy contacts...\n");
+	wchar_t buf[128];
+
+	MAHandle list = maPimListOpen(MA_PIM_CONTACTS);
+	TEST(list);
+	int dummyCount = 0;
+	int noteCount = 0;
+	while(1) {
+		MAHandle item = maPimListNext(list);
+		TEST(item);
+		if(item == 0)
+			break;
+		bool isDummy = false;
+		int valueCount;
+		TEST(valueCount = maPimItemFieldCount(item, MA_PIM_FIELD_CONTACT_NOTE));
+		if(valueCount == 1) {
+			noteCount++;
+			MA_PIM_ARGS args;
+			args.item = item;
+			args.field = MA_PIM_FIELD_CONTACT_NOTE;
+			args.buf = buf;
+			args.bufSize = sizeof(buf);
+			TEST(maPimItemGetValue(&args, 0));
+			isDummy = wcscmp((wchar_t*)args.buf, sDummyNote) == 0;
+		}
+		if(isDummy) {
+			printf("Found dummy item!\n");
+			dummyCount++;
+			TEST(maPimItemRemove(list, item));
+		} else {
+			TEST(maPimItemClose(item));
+		}
+		checkEvents();
+	}
+	TEST(maPimListClose(list));
+	printf("Done. %i dummies, %i notes.\n", dummyCount, noteCount);
+}
+#endif
+
 class MyMoblet : public Moblet {
 public:
 	MyMoblet() {
 		printf("Hello PIM!\n");
+#if ADD_REMOVE_ITEMS
+		addContacts();
+#endif
 		listContacts();
+#if ADD_REMOVE_ITEMS
+		removeContacts();
+#endif
 	}
 
 	void keyPressEvent(int keyCode) {
@@ -187,7 +319,7 @@ public:
 
 extern "C" int MAMain() {
 	InitConsole();
-	gConsoleLogging = 0;
+	gConsoleLogging = CONSOLE_LOGGING;
 #if USE_FILE
 	gConsoleFile = maFileOpen("C:/Data/Images/pclog.txt", MA_ACCESS_READ_WRITE);
 	MAASSERT(gConsoleFile > 0);
