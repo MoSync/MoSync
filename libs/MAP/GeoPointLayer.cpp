a1e9ed219d5fa6e6377d369412e0fe6b94819f57
Magnus Hult <magnus@magnushult.se>
2011-01-23 14:39:27 +0100

    Call onContentChanged a bit more often to make the screen redraw properly

--- libs/MAP/GeoPointLayer.cpp
+++ libs/MAP/GeoPointLayer.cpp
@@ -122,25 +122,32 @@ namespace MAP
 	void GeoPointLayer::selectItem( int index )
 	//-------------------------------------------------------------------------
 	{
-		mSelectedItem = index;
+		if (index != mSelectedItem) {
+			mSelectedItem = index;
+			onContentChanged( );
+		}
 	}
 
 	//-------------------------------------------------------------------------
 	void GeoPointLayer::selectNextItem( )
 	//-------------------------------------------------------------------------
 	{
-		mSelectedItem++;
-		if ( mSelectedItem >= mDataSource->size( ) )
-			mSelectedItem = 0;
+		int newSelectedItem = mSelectedItem;
+		newSelectedItem++;
+		if ( newSelectedItem >= mDataSource->size( ) )
+			newSelectedItem = 0;
+		selectItem( newSelectedItem );
 	}
 
 	//-------------------------------------------------------------------------
 	void GeoPointLayer::selectPreviousItem( )
 	//-------------------------------------------------------------------------
 	{
-		mSelectedItem--;
-		if ( mSelectedItem < 0 )
-			mSelectedItem = mDataSource->size( ) - 1;
+		int newSelectedItem = mSelectedItem;
+		newSelectedItem--;
+		if ( newSelectedItem < 0 )
+			newSelectedItem = mDataSource->size( ) - 1;
+		selectItem( newSelectedItem );
 	}
 
 	//-------------------------------------------------------------------------
@@ -167,6 +174,7 @@ namespace MAP
 	{
 		// If there's a point at this pixel coordinate, make it the selected point
 		Enumerator<GeoPoint*> e = Enumerator<GeoPoint*>( *this );
+		int selectedIndex = -1;
 		int index = 0;
 
 		while ( e.moveNext( ) )
@@ -176,10 +184,14 @@ namespace MAP
 
 			if (abs(widgetPixel.x - screenPixel.x) < item->getMarkerSize() &&
 				abs(widgetPixel.y - screenPixel.y) < item->getMarkerSize()) {
-				selectItem(index);
+				selectedIndex = index;
 			}
 
 			index++;
+		}
+
+		if (selectedIndex != -1) {
+			selectItem(selectedIndex);
 		}
 	}
 
