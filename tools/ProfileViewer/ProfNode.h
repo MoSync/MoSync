e734382ac773e07c83753c187f7a1f8d3a61fded
Fredrik Eldh <fredrik.eldh@mobilesorcery.com>
2011-03-05 20:07:57 +0100

    ProfileViewer: can now draw a pie chart.

--- tools/ProfileViewer/ProfNode.h
+++ tools/ProfileViewer/ProfNode.h
@@ -5,21 +5,23 @@ using namespace std;
 
 struct ProfNode;
 
-class sort_ProfNode_totalTime {
+struct sort_ProfNode_totalTime {
 	// return true if x is strictly before y.
 	bool operator()(const ProfNode* x, const ProfNode* y);
 };
+
+typedef set<ProfNode*, sort_ProfNode_totalTime> ProfSet;
 
 struct ProfNode {
 	enum Flags {
 		N, C, CH, T, LT, CHT, _flags_end
 	};
 
-	const ProfNode* parent;
+	ProfNode* const parent;
 	const string name;
 	const int count, nChildren;
 	const float totalTime, childrenTime, localTime;
-	set<ProfNode*, sort_ProfNode_totalTime> children;
+	ProfSet children;
 };
 
 bool sort_ProfNode_totalTime::operator()(const ProfNode* x, const ProfNode* y) {
--- tools/ProfileViewer/pv.cpp
+++ tools/ProfileViewer/pv.cpp
@@ -6,10 +6,12 @@
 #include <fcntl.h>
 #include <expat.h>
 #include <SDL.h>
+#include <vector>
 #include "ProfNode.h"
 #include "FlagCheck.h"
 #include "SDL_gfxPrimitives.h"
 #include "helpers/attribute.h"
+#include "helpers/types.h"
 
 using namespace std;
 
@@ -21,6 +23,8 @@ using namespace std;
 // variables
 static SDL_Surface* sScreen = NULL;
 static ProfNode* sRoot = NULL;
+static ProfNode* sCurrentNode;
+static int sMaxLevel = 0;
 
 // only valid during parsing
 static int sParseLevel, sNodeCount;
@@ -35,6 +39,10 @@ static void ATTRIB(noreturn) fatalError();
 
 static bool streq(const char* a, const char* b) {
 	return strcmp(a, b) == 0;
+}
+
+template<class T> const T& MIN(const T& a, const T& b) {
+	return a < b ? a : b;
 }
 
 static void fatalError() {
@@ -102,17 +110,22 @@ static void XMLCALL opStart(void *data, const char *el, const char **attr) {
 	if(!sCurrentParent) {
 		ASSERT(!sRoot);
 		sRoot = n;
+	} else {
+		sCurrentParent->children.insert(n);
 	}
 	ASSERT(sRoot);
 	sCurrentParent = n;
 	sParseLevel++;
 	//printf("level %i\n", sParseLevel);
 	sNodeCount++;
+	if(sParseLevel > sMaxLevel)
+		sMaxLevel = sParseLevel;
 }
 static void XMLCALL opEnd(void *data, const char *el) {
 	if(!streq(el, "f")) {
 		return;
 	}
+	sCurrentParent = sCurrentParent->parent;
 	sParseLevel--;
 	ASSERT(sParseLevel >= 0);
 }
@@ -154,7 +167,68 @@ void loadProfile(const char* filename) {
 	XML_Parser p = XML_ParserCreate("UTF-8");
 	XML_SetElementHandler(p, opStart, opEnd);
 	parse_xml(p, fd);
-	printf("Loaded %i nodes.\n", sNodeCount);
+	printf("Loaded %i nodes. Max level: %i\n", sNodeCount, sMaxLevel);
+}
+
+struct SLICE {
+	uint color;
+	float fraction;
+};
+static const float TRIGGER_FRACTION = 0.01f;	// 1%
+
+static void dumpNode(ProfNode* node) {
+	printf("%s\n", node->name.c_str());
+}
+
+static void drawPie() {
+	static const uint WHITE = 0xffffffff;
+	static const uint RED = 0xff0000ff;
+	static const uint BLUE = 0x8080ffff;
+	static const uint YELLOW = 0xffff00ff;
+	static const uint PINK = 0xff00ffff;
+	static const uint GREEN = 0x00ff00ff;
+
+	static const uint COLORS[] = {
+		YELLOW, PINK, GREEN,
+	};
+	static const uint nColors = sizeof(COLORS) / sizeof(uint);
+
+	vector<SLICE> slices;
+	uint color = BLUE;
+	{
+		SLICE s = { color, sCurrentNode->localTime / sCurrentNode->totalTime };
+		slices.push_back(s);
+	}
+	uint colorIndex = 0;
+	const ProfSet& ch(sCurrentNode->children);
+	for(ProfSet::iterator itr = ch.begin(); itr != ch.end(); itr++) {
+		const ProfNode* node(*itr);
+		float fraction = node->totalTime / sCurrentNode->totalTime;
+		if(fraction > TRIGGER_FRACTION) {
+			SLICE s = { color, fraction };
+			slices.push_back(s);
+			colorIndex++;
+			if(colorIndex >= nColors)
+				colorIndex = 0;
+		}
+	}
+	printf("%u slices constructed\n", slices.size());
+
+	int centerX = sScreen->w / 2, centerY = sScreen->h / 2;
+	int radius = MIN(centerX, centerY);
+
+	// draw slices
+	Sint16 angle = 0;
+	for(size_t i=0; i<slices.size(); i++) {
+		const SLICE& s(slices[i]);
+		Sint16 nextAngle = (Sint16)(angle + s.fraction * 360);
+		filledPieColor(sScreen, centerX, centerY, radius, angle, nextAngle, s.color);
+		angle = nextAngle;
+	}
+	// draw the rest
+	filledPieColor(sScreen, centerX, centerY, radius, angle, 360, RED);
+
+	SDL_Flip(sScreen);
 }
 
 #undef main
@@ -169,7 +243,8 @@ extern "C" int main(int argc, const char** argv) {
 	SDL_Init(SDL_INIT_VIDEO);
 	sScreen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
 
-	// todo: draw pie
+	sCurrentNode = sRoot;
+	drawPie();
 
 	while(true) {
 		SDL_Event e;
