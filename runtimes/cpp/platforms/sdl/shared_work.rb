81e2d31aacaa616c76b32a36138837a398b55baf
Fredrik Eldh <fredrik.eldh@mobilesorcery.com>
2011-03-22 12:24:35 +0100

    Added an expanded import library for bthprops.cpl. This allows MoRE to perform Bluetooth Service Discovery even when compiled with mingw.

--- runtimes/cpp/platforms/sdl/shared_work.rb
+++ runtimes/cpp/platforms/sdl/shared_work.rb
@@ -9,7 +9,7 @@ def setup_common
 	
 	if(HOST == :win32) then
 		@CUSTOM_LIBS = common_libraries.collect do |lib| "#{lib}.lib" end +
-			["libexpat.lib", "SDL_sound.lib", "libirprops.a", "libuuid.a", "FreeImage.lib",
+			["libexpat.lib", "SDL_sound.lib", "libbthprops_ex.a", "libuuid.a", "FreeImage.lib",
 				"libeay32.lib", "ssleay32.lib"]
 		@LIBRARIES = ["wsock32", "ws2_32"]
 		@LOCAL_DLLS = ["amr"]
