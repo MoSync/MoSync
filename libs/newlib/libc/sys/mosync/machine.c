MoSync/MoSync

commit 91a30e999f114d702fbbd85415706612ed625dc0

Fixed a bug in maFileDate() and maFileTruncate() on UNIX systems when the file has been unlink()'d. Note: this renders Android, iPhone, Symbian and Windows Mobile uncompilable.

runtimes/cpp/base/Syscall.cpp
@@ -705,7 +705,7 @@ namespace Base {
 				fh.fs = new WriteFileStream(fh.name, false, true);
 			}
 		} else if((fh.mode & MA_ACCESS_READ) != 0) {
-			if(res == 0) {
+			if(res >= 0) {	// file exists (may be a directory)
 				fh.fs = new FileStream(fh.name);
 			}
 		} else {
@@ -981,23 +981,12 @@ namespace Base {
 	int Syscall::maFileDate(MAHandle file) {
 		LOGF("maFileDate(%i)\n", file);
 		FileHandle& fh(getFileHandle(file));
-		const char* statName = fh.name;
-		Array<char> temp(0);
-		if(fh.name[fh.name.size()-2] == '/') {
-			// would cause stat() to fail. get rid of the slash.
-			//temp.assign(fh.name, fh.name.size()-2);
-			temp.resize(fh.name.size()-1);
-			memcpy(temp, fh.name, fh.name.size()-2);
-			temp[fh.name.size()-2] = 0;
-			statName = temp;
-		}
-		struct _stat st;
-		if(_stat(statName, &st) != 0) {
-			LOG("maFileDate:stat(%s) failed. errno: %i(%s)\n", statName, errno, strerror(errno));
-			FILE_FAIL(MA_FERR_GENERIC);
-		}
-		LOGF("mtime: %i\n", (int)st.st_mtime);
-		return (int)st.st_mtime;
+		time_t t;
+		if(!fh.fs) FILE_FAIL(MA_FERR_GENERIC);
+		if(!fh.fs->isOpen()) FILE_FAIL(MA_FERR_GENERIC);
+		if(!fh.fs->mTime(t)) FILE_FAIL(MA_FERR_GENERIC);
+		LOGF("mtime: %i\n", (int)t);
+		return (int)t;
 	}
 
 	int Syscall::maFileTruncate(MAHandle file, int offset) {
@@ -1005,27 +994,7 @@ namespace Base {
 		FileHandle& fh(getFileHandle(file));
 		if(!fh.fs) FILE_FAIL(MA_FERR_GENERIC);
 		if(!fh.fs->isOpen()) FILE_FAIL(MA_FERR_GENERIC);
-		// looks like we'll have to close the file, open() it,
-		// truncate it, then restore the FileStream.
-		int oldPos;
-		if(!fh.fs->tell(oldPos)) FILE_FAIL(MA_FERR_GENERIC);
-
-		delete fh.fs;
-		fh.fs = NULL;
-
-		int fd = _open(fh.name, O_RDWR);
-		if(fd < 0) FILE_FAIL(MA_FERR_GENERIC);
-		int res =
-#ifdef WIN32
-		_chsize(fd, offset);
-#else
-		ftruncate(fd, offset);
-#endif
-		if(res < 0) FILE_FAIL(MA_FERR_GENERIC);
-		if(close(fd) < 0) FILE_FAIL(MA_FERR_GENERIC);
-
-		TEST_LTZ(openFile(fh));
-		if(!fh.fs->seek(Seek::Start, MIN(oldPos, offset))) FILE_FAIL(MA_FERR_GENERIC);
+		if(!fh.fs->truncate(offset)) FILE_FAIL(MA_FERR_GENERIC);
 		return 0;
 	}
 #endif	//SYMBIAN && _WIN32_WCE

runtimes/cpp/base/FileStream.h
@@ -44,6 +44,7 @@ namespace Base {
 		virtual bool length(int& aLength) const;
 		virtual bool seek(Seek::Enum mode, int offset);
 		virtual bool tell(int& aPos) const;
+		virtual bool mTime(time_t&) const;
 
 #ifndef _android
 		virtual Stream* createLimitedCopy(int size) const;
@@ -53,6 +54,8 @@ namespace Base {
 		virtual Stream* createCopy() const;
 
 		const char* getFilename() const;
+		
+		virtual bool truncate(int size) { FAIL; }
 
 #include "FileImpl.h"
 	};
@@ -66,6 +69,7 @@ namespace Base {
 		bool write(const void* src, int size);
 		Stream* createLimitedCopy(int /*size*/) const { FAIL; }
 		Stream* createCopy() const { FAIL; }
+		bool truncate(int size);
 	};
 
 	class LimitedFileStream : public FileStream {	//read-only

runtimes/cpp/platforms/sdl/FileImpl.cpp
@@ -121,6 +121,13 @@ namespace Base {
 		LTEST(aPos = lseek(mFd, 0, SEEK_CUR));
 		return true;
 	}
+	bool FileStream::mTime(time_t& t) const {
+		TEST(isOpen());
+		struct stat s;
+		LTEST(fstat(mFd, &s));
+		t = s.st_mtime;
+		return true;
+	};
 
 	//******************************************************************************
 	//LimitedFileStream
@@ -157,6 +164,11 @@ namespace Base {
 			DEBUG_ASSERT(res <= len);
 			pos += res;
 		}
+		return true;
+	}
+	bool WriteFileStream::truncate(int size) {
+		TEST(isOpen());
+		LTEST(ftruncate(mFd, size));
 		return true;
 	}
 

tools/idl2/maapi.idl
@@ -2190,17 +2190,17 @@ interface MAAPI {
 		/**
 		* Truncates the file.
 		*
-		* If offset is less than the file's size,
+		* If \a offset is less than the file's current size,
 		* the file's new size is equal to the offset. All data after the offset is discarded.
 		*
-		* If the offset is equal to the file's size, this function does nothing.
+		* If \a offset is equal to the file's current size, this function does nothing.
 		*
-		* If the offset is greater than the file's current size,
+		* If \a offset is greater than the file's current size,
 		* this function MAY increase the size of the file.
 		* You can determine if it did so by calling maFileSeek() with #MA_SEEK_END.
 		*
-		* If the file's position is greater than the offset, it is reset
-		* to be equal to the offset.
+		* If the file's current position is greater than \a offset, it is reset
+		* to be equal to \a offset.
 		*
 		* The file must exist prior to calling this function.
 		* Returns 0 on success, or \< 0 on error.

commit ed9fb53bb8d768a5e42bcc2ffe5a412af40887a3

Fixed maFileOpen() of directories on Windows. The problem was introduced by commit 91a30e999f114d702fbbd85415706612ed625dc0. FileStream cannot open directories on Windows. That could be changed, but only by using CreateFile() & co. We don't need that yet, I think.

libs/newlib/libc/sys/mosync/machine.c
@@ -146,7 +146,7 @@ static void initFda(void) {
 static struct LOW_FD* getLowFd(int __fd) {
 	initFda();
 	//LOGD("getLowFd(%i)", __fd);
-	if(__fd <= 0 || __fd >= NFD) {
+	if(__fd < 0 || __fd >= NFD) {
 		errno = EBADF;
 		return NULL;
 	}

runtimes/cpp/base/Syscall.cpp
@@ -705,7 +705,7 @@ namespace Base {
 				fh.fs = new WriteFileStream(fh.name, false, true);
 			}
 		} else if((fh.mode & MA_ACCESS_READ) != 0) {
-			if(res >= 0) {	// file exists (may be a directory)
+			if(res == 0) {	//file exists and is not a directory
 				fh.fs = new FileStream(fh.name);
 			}
 		} else {
@@ -982,9 +982,27 @@ namespace Base {
 		LOGF("maFileDate(%i)\n", file);
 		FileHandle& fh(getFileHandle(file));
 		time_t t;
-		if(!fh.fs) FILE_FAIL(MA_FERR_GENERIC);
-		if(!fh.fs->isOpen()) FILE_FAIL(MA_FERR_GENERIC);
-		if(!fh.fs->mTime(t)) FILE_FAIL(MA_FERR_GENERIC);
+		if(fh.fs) {
+			if(!fh.fs->isOpen()) FILE_FAIL(MA_FERR_GENERIC);
+			if(!fh.fs->mTime(t)) FILE_FAIL(MA_FERR_GENERIC);
+		} else {
+			const char* statName = fh.name;
+			Array<char> temp(0);
+			if(fh.name[fh.name.size()-2] == '/') {
+				// would cause stat() to fail. get rid of the slash.
+				//temp.assign(fh.name, fh.name.size()-2);
+				temp.resize(fh.name.size()-1);
+				memcpy(temp, fh.name, fh.name.size()-2);
+				temp[fh.name.size()-2] = 0;
+				statName = temp;
+			}
+			struct _stat st;
+			if(_stat(statName, &st) != 0) {
+				LOG("maFileDate:stat(%s) failed. errno: %i(%s)\n", statName, errno, strerror(errno));
+				FILE_FAIL(MA_FERR_GENERIC);
+			}
+			t = st.st_mtime;
+		}
 		LOGF("mtime: %i\n", (int)t);
 		return (int)t;
 	}

tests/libc-suite/setup_filesystem.c
@@ -136,7 +136,7 @@ static bool tryToMake(const char* dir) {
 	printf("Closing...\n");
 	res = maFileClose(file);
 	MAASSERT(res == 0);
-	printf("Done.\n");
+	printf("Done: %s\n", dir);
 	return true;
 }
 