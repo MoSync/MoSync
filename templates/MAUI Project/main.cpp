f151f8a021df702f60cb635323deb3265c7bbcee
Mikael Kindborg <mikael.kindborg@mosync.com>
2011-05-18 13:05:27 +0200

    Templates: Updated project templates.

--- templates/MAUI Project/main.cpp
+++ templates/MAUI Project/main.cpp
@@ -5,47 +5,141 @@
 using namespace MAUtil;
 using namespace MAUI;
 
-class MyScreen : public Screen {
+/**
+ * MAUI is short for MoSync API User Interface.
+ *
+ * A Screen is a MAUI object that holds widgets.
+ */
+class MAUIScreen : public Screen
+{
 public:
-	MyScreen() {
-		/* 
-			todo: initialize the widget hierarchy of this screen and possibly show it
-			example:
-				Label* l = new Label(0, 0, 50, 50, NULL, "", 0x1f7f1f, NULL);
-				setMain(l);		
-		*/
+	/**
+	 * Create widgets in the constructor.
+	 */
+	MAUIScreen()
+	{
+		mBackgroundArea = new Label(
+			0,   // Left
+			0,   // Top
+			50,  // Width
+			50,  // Height
+			NULL // Parent widget
+			);
+		mBackgroundArea->setBackgroundColor(0xFFFFFF);
+		setMain(mBackgroundArea);
+
+		mTouchArea = new Label(
+			0,   // Left
+			0,   // Top
+			50,  // Width
+			50,  // Height
+			mBackgroundArea // Parent widget
+			);
+		mTouchArea->setBackgroundColor(0x000000);
 	}
-	
-	~MyScreen() {
-		// todo: delete main widget of this screen 
+
+	/**
+	 * Deallocate objects in the destructor.
+	 */
+	virtual ~MAUIScreen()
+	{
+		delete mBackgroundArea;
+		delete mTouchArea;
 	}
+
+	/**
+	 * Called when a key is pressed.
+	 */
+	virtual void keyPressEvent(int keyCode, int nativeCode)
+	{
+		// Default color is white.
+		int color = 0xFFFFFF;
+
+		switch (keyCode)
+		{
+			case MAK_1:
+				color = 0xFF5555;
+				break;
+			case MAK_2:
+				color = 0x55FF55;
+				break;
+			case MAK_3:
+				color = 0x5555FF;
+				break;
+		}
+
+		// Set the new background color.
+		mBackgroundArea->setBackgroundColor(color);
+	}
+
+	/**
+	 * Called when the screen is touched.
+	 */
+	virtual void pointerPressEvent(MAPoint2d point)
+	{
+		// Center the touch area at the pointer position.
+		mTouchArea->setPosition(point.x - 25, point.y - 25);
+
+		// We need to repaint the parent when a child
+		// widget is changed.
+		mBackgroundArea->requestRepaint();
+	}
+
 private:
+	Label* mBackgroundArea;
+	Label* mTouchArea;
 };
 
-class MAUIMoblet : public Moblet {
+/**
+ * A Moblet is a high-level class that defines the
+ * behaviour of a MoSync program.
+ *
+ * To use MAUI you need a Moblet, but a  Moblet can
+ * be used with or without the MAUI library.
+ */
+class MAUIMoblet : public Moblet
+{
 public:
-	MAUIMoblet() {
-		// initialize
-		screen = new MyScreen();
-		screen->show();
+	/**
+	 * Initialize the application in the constructor.
+	 */
+	MAUIMoblet()
+	{
+		mScreen = new MAUIScreen();
+		mScreen->show();
 	}
 
-	void keyPressEvent(int keyCode, int nativeCode) {
-		// todo: handle key presses
+	/**
+	 * Deallocate objects in the destructor.
+	 */
+	virtual ~MAUIMoblet()
+	{
+		delete mScreen;
 	}
 
-	void keyReleaseEvent(int keyCode, int nativeCode) {
-		// todo: handle key releases
+	/**
+	 * Called when a key is pressed.
+	 */
+	void keyPressEvent(int keyCode, int nativeCode)
+	{
+		if (MAK_BACK == keyCode || MAK_0 == keyCode)
+		{
+			// Call close to exit the application.
+			close();
+		}
 	}
-	MyScreen* screen;
 
-	~MAUIMoblet() {
-		delete screen;
-	}
-	
+private:
+	MAUIScreen* mScreen;
 };
 
-extern "C" int MAMain() {
+/**
+ * Entry point of the program. The MAMain function
+ * needs to be declared as extern "C".
+ */
+extern "C" int MAMain()
+{
 	Moblet::run(new MAUIMoblet());
 	return 0;
-};
+}
+
