ea9ee4800b882e4050e7b0bdf6b50de8412eb45f
Mikael Kindborg <mikael.kindborg@mosync.com>
2011-06-01 08:24:02 +0200

    Examples: Updated the panic notification text of the RockPaperScissors game, and added the program to the examples list.

--- examples/RockPaperScissorsGameNativeUI/PlayScreen.cpp
+++ examples/RockPaperScissorsGameNativeUI/PlayScreen.cpp
@@ -544,8 +544,8 @@ void PlayScreen::setupUI()
 	if (-1 == mScreen)
 	{
 		maPanic(0,
-			"This application uses NativeUI, which currently is "
-			"supported only on Android and iOS devices.");
+			"NativeUI is only available on Android and iPhone. "
+			"You must run directly on the device or devices emulator.");
 	}
 
 	// Create the main layout.
