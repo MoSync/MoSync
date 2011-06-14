68d2fb2cc5a338c8cdd81b47d5acea081bd8fd41
Niklas Nummelin <niklas@mosync.com>
2011-06-14 11:44:50 +0200

    android, changed so that pop-events is sent even when programatically popping a screen stack. Using the back button it is now not possible to pop the last item on the screen stack (this is only possible to remove programatically).

--- runtimes/java/platforms/androidJNI/AndroidProject/src/com/mosync/nativeui/ui/widgets/StackScreenWidget.java
+++ runtimes/java/platforms/androidJNI/AndroidProject/src/com/mosync/nativeui/ui/widgets/StackScreenWidget.java
@@ -78,6 +78,25 @@ public class StackScreenWidget extends ScreenWidget
 		getView( ).addView( screen.getView( ) );
 	}
 	
+	private void sendPopEvent() {
+		if( m_screenStack.empty( ) )
+		{
+			return;
+		}
+	
+		// Only pop and send events if the stack is not empty
+		int poppedToScreenHandle = getHandle( );
+		if( m_screenStack.size( ) >= 2 )
+		{
+			poppedToScreenHandle = m_screenStack.get( 1 ).getHandle( );
+		}
+		
+		EventQueue.getDefault( ).postWidgetStackScreenPoppedEvent(
+				getHandle(),
+				m_screenStack.peek( ).getHandle( ),
+				poppedToScreenHandle );	
+	}
+	
 	/**
 	 * Pops a screen from the screen stack, and displays
 	 * the previous screen. If there is no previous screen,
@@ -85,6 +104,8 @@ public class StackScreenWidget extends ScreenWidget
 	 */
 	public void pop()
 	{
+		sendPopEvent();
+	
 		// Remove current view
 		getView( ).removeAllViews( );
 		
@@ -114,22 +135,16 @@ public class StackScreenWidget extends ScreenWidget
 	@Override
 	public boolean handleBack()
 	{
-		if( !m_backEnabled || m_screenStack.empty( ) )
+		if( !m_backEnabled )
 		{
 			return false;
 		}
 		
-		// Only pop and send events if the stack is not empty
-		int poppedToScreenHandle = getHandle( );
-		if( m_screenStack.size( ) >= 2 )
-		{
-			poppedToScreenHandle = m_screenStack.get( 1 ).getHandle( );
+		if(m_screenStack.size( ) <= 1) {
+			return false;
 		}
 		
-		EventQueue.getDefault( ).postWidgetStackScreenPoppedEvent(
-				getHandle(),
-				m_screenStack.peek( ).getHandle( ),
-				poppedToScreenHandle );
+		//sendPopEvent();
 		pop( );
 		
 		return true;
