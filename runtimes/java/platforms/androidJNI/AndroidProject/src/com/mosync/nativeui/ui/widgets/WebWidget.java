22d09ffee0ac05fd372295b546a2f9ccb7336b34
Mikael Kindborg <mikael.kindborg@mosync.com>
2011-06-29 17:02:58 +0200

    Android: Implemented new WebView properties and added content provider support for local file access.

--- runtimes/java/platforms/androidJNI/AndroidProject/src/com/mosync/nativeui/ui/widgets/WebWidget.java
+++ runtimes/java/platforms/androidJNI/AndroidProject/src/com/mosync/nativeui/ui/widgets/WebWidget.java
@@ -1,7 +1,9 @@
 package com.mosync.nativeui.ui.widgets;
 
+import android.app.Activity;
 import android.webkit.WebView;
 
+import com.mosync.internal.android.MoSyncThread;
 import com.mosync.internal.generated.IX_WIDGET;
 import com.mosync.nativeui.util.properties.BooleanConverter;
 import com.mosync.nativeui.util.properties.InvalidPropertyValueException;
@@ -37,33 +39,70 @@ public class WebWidget extends Widget
 	public boolean setProperty(String property, String value)
 			throws PropertyConversionException, InvalidPropertyValueException
 	{
-		if( super.setProperty( property, value ) )
+		if (super.setProperty(property, value))
 		{
 			return true;
 		}
 		
-		WebView webView = (WebView) getView( );
-		if( property.equals( IX_WIDGET.MAW_WEB_VIEW_URL ) )
+		// Get the native Android WebView instance.
+		WebView webView = (WebView) getView();
+		
+		if (property.equals(IX_WIDGET.MAW_WEB_VIEW_URL))
 		{
-			webView.loadUrl( value );
+			// Here we check if the url has schema specifier.
+			// This is done so that is there is no schema
+			// we use the content provider to load the file
+			// from the application's local file system.
+			String url = value;
+			if (url.contains("://") || url.contains("javascript:"))
+			{
+				// There is a schema specified.
+				webView.loadUrl(url);
+			}
+			else
+			{
+				// No schema present, add the content provider 
+				// schema and authority and load the file.
+				Activity activity = MoSyncThread.getInstance().getActivity();
+				url = "content://" + activity.getPackageName() + "/" + url;
+				webView.loadUrl(url);
+			}
 		}
-		else if( property.equals( IX_WIDGET.MAW_WEB_VIEW_NEW_URL ) )
+		else if (property.equals(IX_WIDGET.MAW_WEB_VIEW_NEW_URL))
 		{
 			m_newUrl = value;
 		}
-		else if( property.equals( "html" ) )
+		else if (property.equals("html"))
 		{
+			// Here we specify the base url of our content provider.
+			// See class comment of this class for details:
+			// com.mosync.java.android.MoSyncLocalFileContentProvider.java
+			Activity activity = MoSyncThread.getInstance().getActivity();
 			webView.loadDataWithBaseURL(
-				null, 
+				"content://" + activity.getPackageName() + "/", 
 				value, 
 				"text/html", 
 				"utf-8",
 				null);
 		}
-		else if( property.equals( "enableZoom" ) )
+		else if (property.equals("enableZoom"))
 		{
-			boolean enable = BooleanConverter.convert( value );
+			// Turn on or off zooming.
+			boolean enable = BooleanConverter.convert(value);
 			webView.getSettings().setBuiltInZoomControls(enable);
+		}
+		else if (property.equals("androidAddJavaScriptInterfaceForActivity"))
+		{
+			// Make the string in value a global JavaScript variable
+			// that refers to the application's activity.
+			Activity activity = MoSyncThread.getInstance().getActivity();
+			webView.addJavascriptInterface(activity, value);
+		}
+		else if (property.equals("androidAddJavaScriptInterfaceForWebView"))
+		{
+			// Make the string in value a global JavaScript variable
+			// that refers to this WebView instance.
+			webView.addJavascriptInterface(webView, value);
 		}
 		
 		return true;
@@ -75,18 +114,18 @@ public class WebWidget extends Widget
 	@Override
 	public String getProperty(String property)
 	{
-		WebView webView = (WebView) getView( );
-		if( property.equals( IX_WIDGET.MAW_WEB_VIEW_URL ) )
+		WebView webView = (WebView) getView();
+		if (property.equals(IX_WIDGET.MAW_WEB_VIEW_URL))
 		{
-			return webView.getUrl( );
+			return webView.getUrl();
 		}
-		else if( property.equals( IX_WIDGET.MAW_WEB_VIEW_NEW_URL ) )
+		else if (property.equals(IX_WIDGET.MAW_WEB_VIEW_NEW_URL))
 		{
 			return m_newUrl;
 		}
 		else
 		{
-			return super.getProperty( property );
+			return super.getProperty(property);
 		}
 	}
 }
