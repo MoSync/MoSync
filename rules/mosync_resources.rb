02952e3ceacab275107418298de7faf1e19bfaa3
Fredrik Eldh <fredrik.eldh@mobilesorcery.com>
2011-08-31 17:38:22 +0200

    rules: Fixed capitalization of require 'stringio'.

--- rules/mosync_resources.rb
+++ rules/mosync_resources.rb
@@ -14,7 +14,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-require 'StringIO' # Requires Ruby 1.9.
+require 'stringio'
 require 'rexml/document'
 require 'pathname'
 require "#{File.dirname(__FILE__)}/profiles.rb"
