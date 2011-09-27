2ab26e9ecc523d96ebcb04d34d5873815dd92e0f
Iraklis Rossis <iraklis.rossis@mosync.com>
2011-09-27 16:02:54 +0200

    Finished implementation of maAlert and the EVENT_TYPE_ALERT

--- runtimes/cpp/platforms/iphone/Classes/MoSyncMain.h
+++ runtimes/cpp/platforms/iphone/Classes/MoSyncMain.h
@@ -30,6 +30,8 @@ void MoSync_UpdateView(CGImageRef ref);
 void MoSync_DoneUpdatingView();
 
 void MoSync_ShowMessageBox(const char *title, const char *msg, bool kill);
+
+//Shows an alert box with up to three buttons
 void MoSync_ShowAlert(const char* title, const char* message, const char* button1, const char* button2, const char* button3);
 void MoSync_ShowTextBox(const wchar* title, const wchar* inText, wchar* outText, int maxSize, int constraints);
 
