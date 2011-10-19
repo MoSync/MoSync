527018fcad7f2f23d12ef294d93b42820435cffa
Ali Sarrafi <ali.sarrafi@mosync.com>
2011-10-19 09:19:19 +0200

    [MOSYNCTWOSIX-499] Removed the extra title from the stl documentation link

--- libs/MAStd/api_areas.h
+++ libs/MAStd/api_areas.h
@@ -453,7 +453,8 @@ MAUtil::DataHandler, MAUtil::PlaceholderPool
 <td>MAUtil::Vector, MAUtil::String, MAUtil::HashMap, MAUtil::Map, MAUtil::Set, MAUtil::List,<br>
 \ref MAUtil/Geometry.h, \ref MAUtil/util.h<br>
 </td>
-<td>SGI Standard Template Library (STL) documentation: <a href="http://www.sgi.com/tech/stl/table_of_contents.html" class="el">SGI Standard Template Library (STL) documentation</a>
+<td>
+<a href="http://www.sgi.com/tech/stl/table_of_contents.html" class="el">SGI Standard Template Library (STL) documentation</a>
 </td>
 </tr>
 <tr style="background-color: rgb(249, 203, 156);">
