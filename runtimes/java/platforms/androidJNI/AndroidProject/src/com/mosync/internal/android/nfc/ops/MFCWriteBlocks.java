117222d2c3166bcac9a66220985ce4022ddb051e
Mattias Bybro <mattias.bybro@mosync.com>
2011-10-24 12:35:03 +0200

    LIBS-5: Fixed MiFare UL read/write problems

--- runtimes/java/platforms/androidJNI/AndroidProject/src/com/mosync/internal/android/nfc/ops/MFCWriteBlocks.java
+++ runtimes/java/platforms/androidJNI/AndroidProject/src/com/mosync/internal/android/nfc/ops/MFCWriteBlocks.java
@@ -1,53 +1,30 @@
 package com.mosync.internal.android.nfc.ops;
 
-import static com.mosync.internal.generated.MAAPI_consts.EVENT_TYPE_NFC_TAG_DATA_WRITTEN;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import android.nfc.tech.MifareClassic;
 
 import com.mosync.internal.android.nfc.MifareClassicTag;
-import com.mosync.internal.android.nfc.NFCEvent;
 
-public class MFCWriteBlocks extends TagRunnable<MifareClassicTag> {
+public class MFCWriteBlocks extends MFWrite<MifareClassicTag> {
 
 	private static final int BLOCK_SIZE = 16;
 	private final MifareClassic mifareTag;
-	private final int firstBlock;
-	private final ByteBuffer srcBuffer;
 
 	public MFCWriteBlocks(MifareClassicTag mifareTag, int firstBlock, ByteBuffer srcBuffer) {
-		super(mifareTag, EVENT_TYPE_NFC_TAG_DATA_WRITTEN);
+		super(mifareTag, firstBlock, srcBuffer);
 		this.mifareTag = mifareTag.nativeTag();
-		this.firstBlock = firstBlock;
-		this.srcBuffer = srcBuffer;
 	}
 
 	@Override
-	public NFCEvent doRun() throws IOException {
-		int remaining = srcBuffer.limit();
-		int blockIndex = firstBlock;
-		byte[] block = new byte[BLOCK_SIZE];
-		int writeCount = 0;
-		int blockCount = 0;
-		while (remaining > 0) {
-			int length = Math.min(BLOCK_SIZE, srcBuffer.remaining());
-			if (length < BLOCK_SIZE) {
-				// Just zero it out.
-				block = new byte[BLOCK_SIZE];
-			}
-			srcBuffer.get(block, 0, length);
-			mifareTag.writeBlock(blockIndex + blockCount, block);
-			writeCount += length;
-			remaining -= length;
-			blockCount++;
-		}
-		return new NFCEvent(EVENT_TYPE_NFC_TAG_DATA_WRITTEN, tag.getHandle(), writeCount, this.firstBlock);
+	protected void writeChunk(int chunkIndex, byte[] chunk) throws IOException {
+		mifareTag.writeBlock(chunkIndex, chunk);
 	}
 
 	@Override
-	public String toString() {
-		return String.format("Write %d bytes, starting at block %d of handle %d", srcBuffer.limit(), firstBlock, tag.getHandle());
+	protected int chunkSize() {
+		return BLOCK_SIZE;
 	}
+
 }
