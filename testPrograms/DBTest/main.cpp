c4f0316785f1121ab29119a114acb45a42ae8646
Mikael Kindborg <mikael.kindborg@mosync.com>
2011-11-04 11:48:06 +0100

    [SQLITETWOEIGHT-3] TestPrograms: Updated text program for Database API.

--- testPrograms/DBTest/main.cpp
+++ testPrograms/DBTest/main.cpp
@@ -2,6 +2,9 @@
 #include <conprint.h>
 #include <MAUtil/String.h>
 
+#define MUST_HOLD(condition, errorMessage) \
+	if (!(condition)) { maPanic(0, errorMessage); }
+
 /**
  * Get the path to the local file system.
  * @return Path that ends with a slash.
@@ -26,65 +29,167 @@ MAUtil::String getLocalPath()
 }
 
 /**
- * Utility function that zero terminates the column text.
+ * Utility function that gets a text zero terminated.
  */
-void getString(MAHandle cursor, int column, char* buf, int maxSize)
+void getColumnString(MAHandle cursor, int column, char* buf, int maxSize)
 {
-   int size = maDBCursorGetColumnText(cursor, column, buf, maxSize);
-   if (size < maxSize)
-   {
-       buf[size] = 0;
-   }
-   else
-   {
-       buf[maxSize] = 0;
-   }
+	int size = maDBCursorGetColumnText(cursor, column, buf, maxSize);
+	MUST_HOLD(MA_DB_ERROR != size, "getColumnString failed");
+	if (size < maxSize)
+	{
+		buf[size] = 0;
+	}
+	else
+	{
+		buf[maxSize] = 0;
+	}
+}
+
+/**
+ * Utility function that gets an int value.
+ */
+int getColumnInt(MAHandle cursor, int column)
+{
+	int value;
+	int success = maDBCursorGetColumnInt(cursor, column, &value);
+	MUST_HOLD(MA_DB_OK == success, "getColumnInt failed");
+	return value;
+}
+
+/**
+ * Utility function that gets a float value.
+ */
+float getColumnFloat(MAHandle cursor, int column)
+{
+	float value;
+	int success = maDBCursorGetColumnFloat(cursor, column, &value);
+	MUST_HOLD(MA_DB_OK == success, "getColumnInt failed");
+	return value;
 }
 
 /**
  * Test the database syscalls.
  */
-void testDB()
+void test1()
 {
+	int result;
+
+	printf("Test 1 started\n");
+
 	// Open/create the database.
 	MAUtil::String path = getLocalPath();
 	path += "MikiDB";
+	printf("Database path: %s\n", path.c_str());
 	MAHandle db = maDBOpen(path.c_str());
-	if (db < 0)
-	{
-		maPanic(0, "Could not open database.");
-	}
+	MUST_HOLD(db > 0, "maDBOpen failed");
 
 	// TODO: Check all return values.
 
-	// Create a new table, drop the table if it exists.
-	maDBExecSQL(db, "DROP TABLE person");
-	maDBExecSQL(db,
-	   "CREATE TABLE person (name TEXT(50), phone TEXT(30), age INTEGER)");
+	// Create a new table, first drop the table if it exists.
+	maDBExecSQL(db, "DROP TABLE pet");
+
+	// This drop table should always fail.
+	result = maDBExecSQL(db, "DROP TABLE pet");
+	MUST_HOLD(MA_DB_ERROR == result, "DROP TABLE should have failed");
+
+	// Create table.
+	result = maDBExecSQL(db,
+		"CREATE TABLE pet (name TEXT(50), age INTEGER, curiosity FLOAT)");
+	MUST_HOLD(MA_DB_OK == result, "CREATE TABLE failed");
 
 	// Insert values.
-	maDBExecSQL(db, "INSERT INTO person VALUES ('Micke', '0707602631', 49)");
-	maDBExecSQL(db, "INSERT INTO person VALUES ('Ali', 'xxxxxxxxx', 1)");
+	result = maDBExecSQL(db, "INSERT INTO pet VALUES ('Kurre', 13, 0.75)");
+	MUST_HOLD(MA_DB_OK == result, "INSERT 1 failed");
+	result = maDBExecSQL(db, "INSERT INTO pet VALUES ('Vilma', 10, 0.999)");
+	MUST_HOLD(MA_DB_OK == result, "INSERT 2 failed");
 
 	// Query all rows.
-	MAHandle cursor = maDBExecSQL(db, "SELECT * FROM person");
-	printf("Number of rows %s\n" , maDBCursorGetRowCount(cursor));
+	MAHandle cursor = maDBExecSQL(db, "SELECT * FROM pet");
+	printf("Number of rows: %i\n" , maDBCursorGetRowCount(cursor));
+	MUST_HOLD(2 == maDBCursorGetRowCount(cursor), "Wrong number of rows");
 
 	// Print all rows.
 	char name[51];
-	char phone[31];
-	char age[11];
-	while (-1 != maDBCursorNext(cursor))
+	int age;
+	float curiosity;
+	int row = 0;
+	while (MA_DB_OK == maDBCursorNext(cursor))
 	{
-	   getString(cursor, 0, name, 50);
-	   getString(cursor, 1, phone, 30);
-	   getString(cursor, 2, age, 10);
-	   printf("%s %s %s\n", name, phone, age);
-	}
-	maDBCursorDestroy(cursor);
-	maDBClose(db);
+		// Get and print data.
+		getColumnString(cursor, 0, name, 50);
+		age = getColumnInt(cursor, 1);
+		curiosity = getColumnFloat(cursor, 2);
+		printf("%s %d %f\n", name, age, curiosity);
 
-	printf("Test passed successfully\n");
+		// Test that data is as expected.
+		++row;
+		if (1 == row)
+		{
+			MUST_HOLD(0 == strcmp(name, "Kurre"), "name Kurre failed");
+			MUST_HOLD(13 == age, "age 13 failed");
+			MUST_HOLD(0.75 == curiosity, "curiosity 0.75 failed");
+		}
+		if (2 == row)
+		{
+			MUST_HOLD(0 == strcmp(name, "Vilma"), "name Vilma failed");
+			MUST_HOLD(10 == age, "age 10 failed");
+			// Float comparisons are tricky...
+			MUST_HOLD(0.998 < curiosity && 0.9991 > curiosity,
+				"curiosity 0.999 failed");
+		}
+	}
+
+	result = maDBCursorDestroy(cursor);
+	MUST_HOLD(MA_DB_OK == result, "maDBCursorDestroy failed");
+
+	result = maDBClose(db);
+	MUST_HOLD(MA_DB_OK == result, "maDBClose failed");
+
+	printf("Test 1 passed successfully\n");
+}
+
+
+/**
+ * Negative tests.
+ */
+void test2()
+{
+	int result;
+
+	printf("Test 2 started\n");
+
+	// Open/create the database.
+	MAUtil::String path = getLocalPath();
+	path += "MikiDB";
+	printf("Database path: %s\n", path.c_str());
+	MAHandle db = maDBOpen(path.c_str());
+	// Should succeed.
+	MUST_HOLD(db > 0, "maDBOpen 2 failed");
+
+	// Should always fail.
+	result = maDBExecSQL(db, "DROP TABLE foo");
+	MUST_HOLD(MA_DB_ERROR == result, "Negative 1 failed");
+
+	// Should always fail.
+	result = maDBExecSQL(9999, "DROP TABLE foo");
+	MUST_HOLD(MA_DB_ERROR == result, "Negative 2 failed");
+
+	result = maDBExecSQL(db, "klasj xasdjkas awjlkasd");
+	MUST_HOLD(MA_DB_ERROR == result, "Negative 3 failed");
+
+	result = maDBClose(db);
+	MUST_HOLD(MA_DB_OK == result, "maDBClose failed");
+
+	MUST_HOLD(MA_DB_ERROR == maDBCursorGetRowCount(999), "Negative 4 failed");
+	MUST_HOLD(MA_DB_ERROR == maDBCursorGetRowCount(0), "Negative 5 failed");
+	MUST_HOLD(MA_DB_ERROR == maDBCursorGetRowCount(-1), "Negative 6 failed");
+
+	result = maDBCursorDestroy(8888);
+	MUST_HOLD(MA_DB_ERROR == maDBCursorGetRowCount(-1), "Negative 7 failed");
+
+	// TODO: Add negative query tests.
+
+	printf("Test 2 passed successfully\n");
 }
 
 /**
@@ -94,8 +199,12 @@ extern "C" int MAMain()
 {
 	MAEvent event;
 
+	// printf("Press zero or back to exit\n");
+	// printf("Touch screen to run DB test\n");
+
+	test1();
+	test2();
 	printf("Press zero or back to exit\n");
-	printf("Touch screen to run DB test\n");
 
 	while (TRUE)
 	{
@@ -115,7 +224,8 @@ extern "C" int MAMain()
 		}
 		else if (EVENT_TYPE_POINTER_PRESSED == event.type)
 		{
-			testDB();
+//			test1();
+//			test2();
 		}
 	}
 
