5b64aeb5b6a843af4ad556cc6fab8577baf0ba8a
Ciprian Filipas <cipri@mosync.com>
2011-11-07 17:21:58 +0200

    Windows phone 7 vertical layout implementation
    cipri

--- runtimes/csharp/windowsphone/mosync/mosyncRuntime/Source/Modules/NativeUI/MoSyncNativeUIWindowsPhone.cs
+++ runtimes/csharp/windowsphone/mosync/mosyncRuntime/Source/Modules/NativeUI/MoSyncNativeUIWindowsPhone.cs
@@ -263,6 +263,183 @@ namespace MoSync
         }
     }
 
+    //VerticalLayout class
+    public class VerticalLayout : WidgetBaseWindowsPhone
+    {
+        //the Grid that will be used as the VerticalLayout
+        protected System.Windows.Controls.Grid mGrid;
+
+        //padding information
+        protected double mPaddingBottom;
+        protected double mPaddingTop;
+        protected double mPaddingLeft;
+        protected double mPaddingRight;
+
+        //row definitions used as spacers for the padding system
+        protected RowDefinition mSpacerUp;
+        protected RowDefinition mSpacerDown;
+        protected ColumnDefinition mSpacerLeft;
+        protected ColumnDefinition mSpacerRight;
+
+        //the main column of the Grid object (an vertical layout is a grid with one column)
+        protected ColumnDefinition mColDef;
+
+        //the constructor
+        public VerticalLayout()
+        {
+            mGrid = new System.Windows.Controls.Grid();
+
+            mColDef = new ColumnDefinition();
+            mSpacerUp = new RowDefinition();
+            mSpacerDown = new RowDefinition();
+
+            mSpacerRight = new ColumnDefinition();
+            mSpacerLeft = new ColumnDefinition();
+
+            mColDef.Width = new System.Windows.GridLength(1, System.Windows.GridUnitType.Auto);
+
+            mSpacerUp.Height = new System.Windows.GridLength(1, System.Windows.GridUnitType.Auto);
+            mSpacerDown.Height = new System.Windows.GridLength(1, System.Windows.GridUnitType.Auto);
+            mSpacerRight.Width = new System.Windows.GridLength(1, System.Windows.GridUnitType.Auto);
+            mSpacerLeft.Width = new System.Windows.GridLength(1, System.Windows.GridUnitType.Auto);
+
+            mGrid.RowDefinitions.Add(mSpacerUp);
+            mGrid.RowDefinitions.Add(mSpacerDown);
+
+            mGrid.ColumnDefinitions.Add(mSpacerLeft);
+            mGrid.ColumnDefinitions.Add(mColDef);
+            mGrid.ColumnDefinitions.Add(mSpacerRight);
+
+            mView = mGrid;
+
+            //mGrid.ShowGridLines = true; //uncomment to see the grid lines (for debugging)
+
+            ChildVerticalAlignment = MoSync.Constants.MAW_ALIGNMENT_TOP;
+            ChildHorizontalAlignment = MoSync.Constants.MAW_ALIGNMENT_LEFT;
+        }
+
+        //add child
+        public override void AddChild(IWidget child)
+        {
+            base.AddChild(child);
+            MoSync.Util.RunActionOnMainThreadSync(() =>
+            {
+                WidgetBaseWindowsPhone widget = (child as WidgetBaseWindowsPhone);
+                RowDefinition rowDef = new RowDefinition();
+
+                rowDef.Height = new System.Windows.GridLength(1, System.Windows.GridUnitType.Auto);
+                mGrid.RowDefinitions.Insert(mGrid.RowDefinitions.Count - 1, rowDef);
+
+                mGrid.Children.Add(widget.View);
+
+                Grid.SetColumn((widget.View as System.Windows.FrameworkElement), 1);
+                Grid.SetRow((widget.View as System.Windows.FrameworkElement), mGrid.RowDefinitions.Count - 2);
+            });
+        }
+
+        //MAW_VERTICAL_LAYOUT_CHILD_HORIZONTAL_ALIGNMENT implementation
+        [MoSyncWidgetProperty(MoSync.Constants.MAW_VERTICAL_LAYOUT_CHILD_HORIZONTAL_ALIGNMENT)]
+        public String ChildHorizontalAlignment
+        {
+            set
+            {
+                if (value.Equals(MoSync.Constants.MAW_ALIGNMENT_LEFT))
+                {
+                    mGrid.HorizontalAlignment = HorizontalAlignment.Left;
+                    mSpacerRight.Width = new GridLength(1, GridUnitType.Star);
+                    mSpacerLeft.Width = new GridLength(0);
+                }
+                else if (value.Equals(MoSync.Constants.MAW_ALIGNMENT_RIGHT))
+                {
+                    mGrid.HorizontalAlignment = HorizontalAlignment.Right;
+                    mSpacerRight.Width = new GridLength(0);
+                    mSpacerLeft.Width = new GridLength(1, GridUnitType.Star);
+                }
+                else if (value.Equals(MoSync.Constants.MAW_ALIGNMENT_CENTER))
+                {
+                    mGrid.HorizontalAlignment = HorizontalAlignment.Center;
+                    mSpacerRight.Width = new GridLength(1, GridUnitType.Star);
+                    mSpacerLeft.Width = new GridLength(1, GridUnitType.Star);
+                }
+            }
+        }
+
+        //MAW_VERTICAL_LAYOUT_CHILD_VERTICAL_ALIGNMENT implementation
+        [MoSyncWidgetProperty(MoSync.Constants.MAW_VERTICAL_LAYOUT_CHILD_VERTICAL_ALIGNMENT)]
+        public String ChildVerticalAlignment
+        {
+            set
+            {
+                if (value.Equals(MoSync.Constants.MAW_ALIGNMENT_BOTTOM))
+                {
+                    mGrid.VerticalAlignment = VerticalAlignment.Bottom;
+                    mSpacerUp.Height = new GridLength(1, GridUnitType.Star);
+                    mSpacerDown.Height = new GridLength(0);
+                }
+                else if (value.Equals(MoSync.Constants.MAW_ALIGNMENT_TOP))
+                {
+                    mGrid.VerticalAlignment = VerticalAlignment.Top;
+                    mSpacerDown.Height = new GridLength(1, GridUnitType.Star);
+                    mSpacerUp.Height = new GridLength(0);
+                }
+                else if (value.Equals(MoSync.Constants.MAW_ALIGNMENT_CENTER))
+                {
+                    mGrid.VerticalAlignment = VerticalAlignment.Center;
+                    mSpacerDown.Height = new GridLength(1, GridUnitType.Star);
+                    mSpacerUp.Height = new GridLength(1, GridUnitType.Star);
+                }
+            }
+        }
+
+        //MAW_VERTICAL_LAYOUT_PADDING_BOTTOM implementation
+        [MoSyncWidgetProperty(MoSync.Constants.MAW_VERTICAL_LAYOUT_PADDING_BOTTOM)]
+        public String PaddingBottom
+        {
+            set
+            {
+                double val = Double.Parse(value);
+                mPaddingBottom = val;
+                mSpacerDown.Height = new GridLength(mPaddingBottom);
+            }
+        }
+
+        //MAW_VERTICAL_LAYOUT_PADDING_TOP implementation
+        [MoSyncWidgetProperty(MoSync.Constants.MAW_VERTICAL_LAYOUT_PADDING_TOP)]
+        public String PaddingTop
+        {
+            set
+            {
+                double val = Double.Parse(value);
+                mPaddingTop = val;
+                mSpacerUp.Height = new GridLength(mPaddingTop);
+            }
+        }
+
+        //MAW_VERTICAL_LAYOUT_PADDING_LEFT implementation
+        [MoSyncWidgetProperty(MoSync.Constants.MAW_VERTICAL_LAYOUT_PADDING_LEFT)]
+        public String PaddingLeft
+        {
+            set
+            {
+                double val = Double.Parse(value);
+                mPaddingLeft = val;
+                mSpacerLeft.Width = new GridLength(mPaddingLeft);
+            }
+        }
+
+        //MAW_VERTICAL_LAYOUT_PADDING_RIGHT implementation
+        [MoSyncWidgetProperty(MoSync.Constants.MAW_VERTICAL_LAYOUT_PADDING_RIGHT)]
+        public String PaddingRight
+        {
+            set
+            {
+                double val = Double.Parse(value);
+                mPaddingRight = val;
+                mSpacerRight.Width = new GridLength(mPaddingRight);
+            }
+        }
+    }
+
     // The button class
     public class Button : WidgetBaseWindowsPhone
     {
@@ -515,8 +692,8 @@ namespace MoSync
             MoSync.Util.RunActionOnMainThreadSync(() =>
             {
                 mPage.Content = w.View;
-                ((System.Windows.FrameworkElement)w.View).Width = 480;
-                ((System.Windows.FrameworkElement)w.View).Height = 800;
+                ((System.Windows.FrameworkElement)w.View).Width = SreenSize.SCREEN_WIDTH;
+                ((System.Windows.FrameworkElement)w.View).Height = SreenSize.SCREEN_HEIGHT;
             });
         }
 
