193dbbb8ab09019fb407806a458ad3ede7d7d92d
Anders Malm <anders@mosync.com>
2011-11-21 21:44:31 +0100

    Android. Disabled the ARM recompiler for Android 1.5 and 1.6

--- tools/ProfileConverter/conv.rb
+++ tools/ProfileConverter/conv.rb
@@ -596,7 +596,7 @@ runtimes.each do |platform_name, platform|
 		end
 
 		release_defines = ['PHONE_RELEASE', 'MOSYNC_COMMERCIAL']
-		if(platform_name == :sp2003 || platform_name == :wm5 || platform_name == :wm6 || platform_name == :wm6pro || platform_name == :s60v3 || platform_name == :s60v5 || platform_name == :android_3 || platform_name == :android_4 || platform_name == :android_7 )
+		if(platform_name == :sp2003 || platform_name == :wm5 || platform_name == :wm6 || platform_name == :wm6pro || platform_name == :s60v3 || platform_name == :s60v5 || platform_name == :android_7 )
 			release_defines << "USE_ARM_RECOMPILER"
 		end
 		if(platform_name == :s60v3)
