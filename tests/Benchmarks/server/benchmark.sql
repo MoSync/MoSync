13e61261cf66b49255303fc3c096edcd4cb084a1
Alexander Samuelsson <alexander.samuelsson@mosync.com>
2011-11-25 14:36:49 +0100

    Added php files with passwords to gitignore. Dumped the database to benchmark.sql and modified publish_result.php to support uploading of membench data.

--- tests/Benchmarks/server/benchmark.sql
+++ tests/Benchmarks/server/benchmark.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: localhost
--- Skapad: 28 okt 2011 kl 13:06
+-- Skapad: 25 nov 2011 kl 12:23
 -- Serverversion: 5.5.16
 -- PHP-version: 5.3.8
 
@@ -39,7 +39,79 @@ CREATE TABLE IF NOT EXISTS `linpack_testruns` (
   KEY `revision` (`revision`),
   KEY `runtime_id` (`runtime_id`),
   KEY `phone_id` (`phone_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+
+--
+-- Dumpning av Data i tabell `linpack_testruns`
+--
+
+INSERT INTO `linpack_testruns` (`id`, `timestamp`, `revision`, `runtime_id`, `git_hash`, `phone_id`, `native_sdk_ver`, `mflops`) VALUES
+(1, '2011-11-03 08:30:38', '1', 3, '1234', 2, '2', 12.3),
+(2, '2011-11-03 08:31:34', '1', 3, '1234', 2, '', 12.3),
+(3, '2011-11-03 08:46:50', '1', 0, '1234', 0, '', 12.3),
+(4, '2011-11-03 08:48:28', '1', 0, '1234', 0, '', 12.3),
+(5, '2011-11-03 08:48:43', '1', 0, '1234', 1, '', 12.3),
+(6, '2011-11-03 09:49:44', '1', 1, '1234', 2, '', 12.3),
+(7, '2011-11-03 09:50:41', '1', 1, '1234', 3, '', 12.3),
+(8, '2011-11-03 09:51:02', '1', 1, '1234', 3, '', 12.3),
+(9, '2011-11-03 11:44:38', '1337', 1, '981723987', 3, '2', 12.8),
+(10, '2011-11-03 11:44:44', '1337', 1, '981723987', 3, '2', 12.8),
+(11, '2011-11-03 11:45:55', '1337', 1, '981723987', 3, '2', 100),
+(12, '2011-11-11 09:30:50', '1337', 1, '987123ab', 3, '2', 5.526492),
+(13, '2011-11-11 10:51:59', '1337', 1, '987123ab', 3, '2', 0),
+(14, '2011-11-11 10:54:02', '1337', 1, '987123ab', 3, '2', 0),
+(15, '2011-11-11 10:56:10', '1337', 1, '987123ab', 3, '2', 5.56745),
+(16, '2011-11-11 13:17:52', '1337', 1, '981723987', 3, '2', 12.8),
+(17, '2011-11-11 13:20:54', '1337', 1, '981723987', 3, '2', 12.8),
+(18, '2011-11-11 13:43:30', '1337', 1, '987123ab', 3, '\n2', 5.559705),
+(19, '2011-11-11 16:11:52', '1337', 1, '981723987', 3, '\n2', 1.8),
+(20, '2011-11-11 16:12:59', '1337', 3, '981723987', 3, '\n2', 1.8),
+(21, '2011-11-11 16:13:40', '1337', 1, '987123ab', 3, '\n2', 5.473487),
+(22, '2011-11-11 16:24:18', '1337', 3, '981723987', 3, '\n2', 1.5146714261896888),
+(23, '2011-11-25 10:13:09', '1337', 1, '987123ab', 3, '\n2', 5.611161),
+(24, '2011-11-25 10:19:39', '1337', 1, '987123ab', 3, '\n2', 5.648668),
+(25, '2011-11-25 10:55:21', '1337', 1, '987123ab', 3, '\n2', 5.337);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `membench_testruns`
+--
+
+CREATE TABLE IF NOT EXISTS `membench_testruns` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `revision` varchar(30) NOT NULL,
+  `runtime_id` int(11) NOT NULL,
+  `git_hash` varchar(40) NOT NULL,
+  `phone_id` int(11) NOT NULL,
+  `native_sdk_ver` varchar(50) NOT NULL,
+  `alloc_str_10` double NOT NULL,
+  `alloc_str_100` double NOT NULL,
+  `alloc_void_1` double NOT NULL,
+  `alloc_void_100` double NOT NULL,
+  `alloc_void_1000` double NOT NULL,
+  `alloc_dummy` double NOT NULL,
+  `alloc_dummy_struct` double NOT NULL,
+  `alloc_dummy_mix` double NOT NULL,
+  `access_array` double NOT NULL,
+  `access_vector` double NOT NULL,
+  `add_vector` double NOT NULL,
+  `access_dummy` double NOT NULL,
+  `access_dummy_struct` double NOT NULL,
+  `access_dummy_mix` double NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `revision` (`revision`),
+  KEY `phone_id` (`phone_id`),
+  KEY `runtime_id` (`runtime_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumpning av Data i tabell `membench_testruns`
+--
+
+INSERT INTO `membench_testruns` (`id`, `timestamp`, `revision`, `runtime_id`, `git_hash`, `phone_id`, `native_sdk_ver`, `alloc_str_10`, `alloc_str_100`, `alloc_void_1`, `alloc_void_100`, `alloc_void_1000`, `alloc_dummy`, `alloc_dummy_struct`, `alloc_dummy_mix`, `access_array`, `access_vector`, `add_vector`, `access_dummy`, `access_dummy_struct`, `access_dummy_mix`) VALUES
+(1, '0000-00-00 00:00:00', '1337', 1, '987123ab', 3, '2\n', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -75,7 +147,15 @@ CREATE TABLE IF NOT EXISTS `phones` (
   `phone_id` int(11) NOT NULL AUTO_INCREMENT,
   `phone_name` varchar(30) NOT NULL,
   PRIMARY KEY (`phone_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumpning av Data i tabell `phones`
+--
+
+INSERT INTO `phones` (`phone_id`, `phone_name`) VALUES
+(1, 'xperia'),
+(3, 'HTC Wildfire');
 
 -- --------------------------------------------------------
 
--- tests/Benchmarks/server/publish_result.php
+++ tests/Benchmarks/server/publish_result.php
@@ -7,15 +7,40 @@ if(!isset($_GET['benchmark'], $_GET['phone'], $_GET['runtime']))
 	die('All required GET parameters was not sent!');
 
 /* store the input retrieved via HTTP GET */
-$benchmark = mysql_real_escape_string($_GET['benchmark']); //which benchmark we ran 'linpack', 'opengl', etc.
-$revision = mysql_real_escape_string($_GET['revision']); //which revision of mosync we ran it on (if we did not run a native runtime)
-$runtime = mysql_real_escape_string($_GET['runtime']); //the runtime name, must be matched to a runtime_id
-$git_hash = mysql_real_escape_string($_GET['git_hash']); //the git_hash of the current build (if running a mosync runtime or mosync generated native code)
-$phone = mysql_real_escape_string($_GET['phone']); //the phone name, must be matched to a phone_id 
-$native_sdk_ver = mysql_real_escape_string($_GET['native_sdk_ver']); //the native SDK version
-$mflops = mysql_real_escape_string($_GET['mflops']); //the linpack result in MFLOPS (float)
+$benchmark = mysql_real_escape_string($_GET['benchmark']); 				//which benchmark we ran 'linpack', 'opengl', etc.
+$revision = mysql_real_escape_string($_GET['revision']); 				//which revision of mosync we ran it on (if we did not run a native runtime)
+$runtime = mysql_real_escape_string($_GET['runtime']); 					//the runtime name, must be matched to a runtime_id
+$git_hash = mysql_real_escape_string($_GET['git_hash']); 				//the git_hash of the current build (if running a mosync runtime or mosync generated native code)
+$phone = mysql_real_escape_string($_GET['phone']); 						//the phone name, must be matched to a phone_id 
+$native_sdk_ver = mysql_real_escape_string($_GET['native_sdk_ver']);	//the native SDK version
 
-$test = mysql_real_escape_string($_GET['test']); //additional parameters
+//only access the $_GET indexes we use
+if($benchmark == 'linpack'){
+	/* LINPACK PARAMETERS */
+	$mflops = mysql_real_escape_string($_GET['mflops']); 					//the linpack result in MFLOPS (float)
+}else if($benchmark == 'opengl'){
+	/* OpenGLES PARAMETERS*/
+	$test1 = mysql_real_escape_string($_GET['test1']); 						//OpenGLES test #1
+	$test2 = mysql_real_escape_string($_GET['test2']); 						//OpenGLES test #2
+	$test3 = mysql_real_escape_string($_GET['test3']); 						//OpenGLES test #3
+	$test4 = mysql_real_escape_string($_GET['test4']); 						//OpenGLES test #4
+}else if($benchmark == 'membench'){
+	/* MEMBENCH PARAMETERS */
+	$alloc_str_10 = mysql_real_escape_string($_GET['alloc_str_10']);
+	$alloc_str_100 = mysql_real_escape_string($_GET['alloc_str_100']);
+	$alloc_void_1 = mysql_real_escape_string($_GET['alloc_void_1']);
+	$alloc_void_100 = mysql_real_escape_string($_GET['alloc_void_100']);
+	$alloc_void_1000 = mysql_real_escape_string($_GET['alloc_void_1000']);
+	$alloc_dummy = mysql_real_escape_string($_GET['alloc_dummy']);
+	$alloc_dummy_struct = mysql_real_escape_string($_GET['alloc_dummy_struct']);
+	$alloc_dummy_mix = mysql_real_escape_string($_GET['alloc_dummy_mix']);
+	$access_array = mysql_real_escape_string($_GET['access_array']);
+	$access_vector = mysql_real_escape_string($_GET['access_vector']);
+	$add_vector = mysql_real_escape_string($_GET['add_vector']);
+	$access_dummy = mysql_real_escape_string($_GET['access_dummy']);
+	$access_dummy_struct = mysql_real_escape_string($_GET['access_dummy_struct']);
+	$access_dummy_mix = mysql_real_escape_string($_GET['access_dummy_mix']);
+}
 
 //echo(" $benchmark $revision $runtime_id $git_hash $phone_id $native_sdk_ver $mflops"); //DEBUG prints to see the values of the GET params
 
@@ -45,9 +70,16 @@ if(mysql_num_rows($res) == 0){
 }
 
 if($benchmark == 'linpack'){ //insert row into 'linpack_testruns'
-	$res = mysql_query("INSERT INTO linpack_testruns (revision, runtime_id, git_hash, phone_id, native_sdk_ver, mflops) VALUES ('$revision', '$runtime_id', '$git_hash', '$phone_id', '$native_sdk_ver', '$mflops')");
-}else if($benchmark == 'opengl'){ //insert row into 'opengl_testruns'
-	$res = mysql_query("INSERT INTO opengl_testruns (revision, runtime_id, git_hash, phone_id, native_sdk_ver, mflops) VALUES ('$revision', '$runtime_id', '$git_hash', '$phone_id', '$native_sdk_ver', '$mflops')");
+	$res = mysql_query("INSERT INTO linpack_testruns (revision, runtime_id, git_hash, phone_id, native_sdk_ver, mflops) VALUES ('$revision', '$runtime_id', '$git_hash', '$phone_id', '
+$native_sdk_ver', '$mflops')");
+}else if($benchmark == 'opengl'){ //insert row into 'opengl_testruns' TODO FIX
+	$res = mysql_query("INSERT INTO opengl_testruns (revision, runtime_id, git_hash, phone_id, native_sdk_ver, mflops) VALUES ('$revision', '$runtime_id', '$git_hash', '$phone_id', '$native_sdk_ver
+', '$mflops')");
+}else if($benchmark == 'membench'){
+	$res = mysql_query("INSERT INTO membench_testruns (revision, runtime_id, git_hash, phone_id, native_sdk_ver, alloc_str_10, alloc_str_100, alloc_void_1, alloc_void_100, alloc_void_1000, 
+	alloc_dummy, alloc_dummy_struct, alloc_dummy_mix, access_array, access_vector, add_vector, access_dummy, access_dummy_struct, access_dummy_mix) VALUES ('$revision', '$runtime_id', '$git_hash', '
+	$phone_id', '$native_sdk_ver', '$alloc_str_10', '$alloc_str_100', '$alloc_void_1', '$alloc_void_100', '$alloc_void_1000', '$alloc_dummy', '$alloc_dummy_struct', '$alloc_dummy_mix', '$access_array
+	', '$access_vector', '$add_vector', '$access_dummy', '$access_dummy_struct', '$access_dummy_mix')");
 }else{ //die, no matching table for the benchmark param
 	die('benchmark was not correctly set in the GET request');
 }
