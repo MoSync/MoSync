c3089c74bba078c6f10e2a493108e3233eca575e
Ciprian Filipas <cipri@mosync.com>
2011-11-28 16:24:59 +0200

    WP7: added the libs to the csproj file and added the MoSyncTabScreen.cs

---
+++ runtimes/csharp/windowsphone/mosync/mosyncRuntime/Source/Modules/NativeUI/MoSyncTabScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MoSync
+{
+    namespace NativeUI
+    {
+        public class TabScreen : Screen
+        {
+            protected Microsoft.Phone.Controls.Pivot mPivot;
+
+            public TabScreen()
+            {
+                mPivot = new Microsoft.Phone.Controls.Pivot();
+                mPivot.Title = "My Application";
+                View = mPivot;
+            }
+
+            public override void AddChild(IWidget child)
+            {
+                base.AddChild(child);
+                if (child is Screen)
+                {
+                    MoSync.Util.RunActionOnMainThreadSync(() =>
+                        {
+                            Microsoft.Phone.Controls.PivotItem pivItem = new Microsoft.Phone.Controls.PivotItem();
+                            pivItem.Height = 200;
+                            //pivItem.Content = child;
+                            pivItem.Header = ((child as Screen).View as Microsoft.Phone.Controls.PhoneApplicationPage).Title;
+                            mPivot.Items.Add(pivItem);
+                        }
+                    );
+                }
+            }
+        }
+    }
+}
