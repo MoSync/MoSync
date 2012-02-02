8fd1d1e0a4a0b33f3b65fc70fa4270c70241a3bf
Ali Sarrafi <ali.sarrafi@mosync.com>
2012-02-02 17:29:12 +0100

    [MOSYNC-1702] Fixed issues with sensorManager documentation.

--- libs/Wormhole/jslib/buildJSDocumentation.rb
+++ libs/Wormhole/jslib/buildJSDocumentation.rb
@@ -17,14 +17,13 @@ pg_files.each { |fileName|
   end
     }
 
-
 puts "running converter"
 system "ruby joDocConverter.rb"
 FileUtils.cp "template/WHTemplate.html", "tempDocs"
 FileUtils.cp "mdDocs/toc.md", "tempDocs"
 
 FileUtils.cd("tempDocs")
-system("perl ../../../../tools/ReleasePackageBuild/JoDoc/jodoc --output ../html5  --title \"Wormhole API Documentation\" --template WHTemplate.html --toc toc.md --markdown ../../../../tools/ReleasePackageBuild/JoDoc/Markdown.pl *.js *.md")
+system("../../../../tools/ReleasePackageBuild/JoDoc/jodoc --output ../html5  --title \"Wormhole API Documentation\" --template WHTemplate.html --toc toc.md --markdown ../../../../tools/ReleasePackageBuild/JoDoc/Markdown.pl *.js *.md")
 
 FileUtils.cd("../")
 style_list = Dir["template/**"]
