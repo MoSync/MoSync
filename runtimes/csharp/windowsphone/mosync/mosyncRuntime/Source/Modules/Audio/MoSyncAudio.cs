b8d339a7cd4117e48630798582431a7a10719f95
Niklas Nummelin <nummelin@gmail.com>
2012-03-21 15:27:54 +0100

    Windows Phone, added initial Audio API implementation (With a few missing features as the Audio API on Windows Phone is rather limited).

---
+++ runtimes/csharp/windowsphone/mosync/mosyncRuntime/Source/Modules/Audio/MoSyncAudio.cs
@@ -0,0 +1,327 @@
+﻿/* Copyright (C) 2012 MoSync AB
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+*/
+
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System;
+using System.IO;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace MoSync
+{
+	class AudioDataSoundEffect : IAudioData
+	{
+		private SoundEffect mSoundEffect = null;
+
+		public AudioDataSoundEffect(SoundEffect soundEffect)
+		{
+			this.mSoundEffect = soundEffect;
+		}
+
+		public IAudioInstance CreateInstance()
+		{
+			return new AudioInstanceSoundEffect(mSoundEffect);
+		}
+
+		public void Dispose()
+		{
+			if (mSoundEffect != null)
+			{
+				mSoundEffect.Dispose();
+			}
+
+			mSoundEffect = null;
+		}
+	}
+
+	class AudioInstanceSoundEffect : IAudioInstance
+	{
+		private SoundEffectInstance mSoundEffectInstance = null;
+		private SoundEffect mSoundEffect = null;
+
+		public AudioInstanceSoundEffect(SoundEffect soundEffect)
+		{
+			this.mSoundEffect = soundEffect;
+			this.mSoundEffectInstance = soundEffect.CreateInstance();
+		}
+
+		public void Play()
+		{
+			mSoundEffectInstance.Play();
+		}
+
+		public void Stop()
+		{
+			mSoundEffectInstance.Stop();
+		}
+
+		public void Pause()
+		{
+			mSoundEffectInstance.Pause();
+		}
+
+		public void SetPosition(int _millis)
+		{
+			if (_millis == 0)
+			{
+				SoundState prevState = mSoundEffectInstance.State;
+				Stop();
+				if (prevState == SoundState.Playing)
+				{
+					Play();
+				}
+
+				return;
+			}
+
+			throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_GENERIC);
+		}
+
+		public int GetPosition()
+		{
+			throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_GENERIC);
+		}
+
+		public int GetLength()
+		{
+			return (int)mSoundEffect.Duration.TotalMilliseconds;
+		}
+
+		public void SetVolume(float vol)
+		{
+			mSoundEffectInstance.Volume = vol;
+		}
+
+		public void SetNumberOfLoops(int numLoops)
+		{
+			throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_GENERIC);
+		}
+
+		public void Dispose()
+		{
+			if (mSoundEffectInstance != null)
+			{
+				mSoundEffectInstance.Dispose();
+			}
+
+			mSoundEffectInstance = null;
+		}
+
+		public void Prepare(Action async)
+		{
+			if (async != null)
+			{
+				async();
+			}
+		}
+	}
+
+	class AudioDataMediaPlayer : IAudioData
+	{
+		private Uri mPathToMedia = null;
+
+		public AudioDataMediaPlayer(Uri pathToMedia)
+		{
+			this.mPathToMedia = pathToMedia;
+		}
+
+		public IAudioInstance CreateInstance()
+		{
+			if(mPathToMedia == null)
+			{
+				throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_INVALID_DATA);
+			}
+
+			return new AudioInstanceMediaPlayer(mPathToMedia);
+		}
+
+		public void Dispose()
+		{
+			mPathToMedia = null;
+		}
+	}
+
+	class AudioInstanceMediaPlayer : IAudioInstance
+	{
+		private Uri mUri = null;
+		private Song mSong = null;
+
+		public AudioInstanceMediaPlayer(Uri uri)
+		{
+			this.mUri = uri;
+			CreateSong();
+		}
+
+		private void CreateSong()
+		{
+			this.mSong = Song.FromUri(mUri.ToString(), mUri);
+		}
+
+		public void Play()
+		{
+			if (mSong == null)
+			{
+				CreateSong();
+			}
+
+			if (MediaPlayer.Queue.ActiveSong != mSong)
+			{
+				MediaPlayer.Stop();
+			}
+
+			MediaPlayer.Play(mSong);
+		}
+
+		public void Stop()
+		{
+			MediaPlayer.Stop();
+			if (mSong != null)
+			{
+				Dispose();
+			}
+		}
+
+		public void Pause()
+		{
+			MediaPlayer.Pause();
+		}
+
+		public void SetPosition(int _millis)
+		{
+			if (_millis == 0)
+			{
+				MediaState prevState = MediaPlayer.State;
+				Stop();
+				if (prevState == MediaState.Playing)
+				{
+					Play();
+				}
+
+				return;
+			}
+
+			throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_GENERIC);
+		}
+
+		public int GetPosition()
+		{
+			throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_GENERIC);
+		}
+
+		public int GetLength()
+		{
+			if (mSong == null)
+			{
+				throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_INVALID_DATA);
+			}
+
+			return (int)mSong.Duration.TotalMilliseconds;
+		}
+
+		public void SetVolume(float vol)
+		{
+			MediaPlayer.Volume = vol;
+		}
+
+		public void SetNumberOfLoops(int numLoops)
+		{
+			throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_GENERIC);
+		}
+
+		public void Dispose()
+		{
+			if (mSong != null)
+			{
+				mSong.Dispose();
+			}
+
+			mSong = null;
+		}
+
+		public void Prepare(Action async)
+		{
+			if (mSong == null)
+			{
+				CreateSong();
+			}
+
+			if (async != null)
+			{
+				async();
+			}
+		}
+	}
+
+	public class Audio
+	{
+		public static IAudioData FromUrlOrFilePath(String url, bool shouldStream)
+		{
+			if (shouldStream)
+			{
+				UriKind uriKind = UriKind.Absolute;
+
+				if (url.StartsWith("file://") == true)
+				{
+					url = url.Replace("file://", "");
+					uriKind = UriKind.Relative;
+				}
+
+				return new AudioDataMediaPlayer(new Uri(url, uriKind));
+			}
+			else
+			{
+				if (url.StartsWith("file://") == true)
+				{
+					String filepath = url.Replace("file://", "");
+					FileModule.File file = new FileModule.File(filepath, FileAccess.Read);
+					file.TryOpen();
+					if (file.FileStream == null)
+					{
+						throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_INVALID_FILE);
+					}
+					IAudioData ret = FromStream(file.FileStream, false);
+					file.Close();
+					return ret;
+				}
+			}
+
+			throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_GENERIC);
+		}
+
+		public static IAudioData FromStream(Stream stream, bool shouldStream)
+		{
+			if (shouldStream)
+			{
+				throw new MoSync.Util.ReturnValueException(MoSync.Constants.MA_AUDIO_ERR_STREAMING_NOT_SUPPORTED);
+			}
+			else
+			{
+				SoundEffect se = SoundEffect.FromStream(stream);
+				return new AudioDataSoundEffect(se);
+			}
+		}
+	}
+}
--- runtimes/csharp/windowsphone/mosync/mosyncRuntime/Source/Modules/MoSyncAudioModule.cs
+++ runtimes/csharp/windowsphone/mosync/mosyncRuntime/Source/Modules/MoSyncAudioModule.cs
@@ -1,5 +1,23 @@
-﻿using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework;
+﻿/* Copyright (C) 2012 MoSync AB
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace MoSync
 {
@@ -14,38 +32,318 @@ namespace MoSync
 			};
 		}
 
+		List<IAudioData> mAudioData = new List<IAudioData>();
+		List<IAudioInstance> mAudioInstances = new List<IAudioInstance>();
+
         public void Init(Ioctls ioctls, Core core, Runtime runtime)
         {
-//			ioctls.maAudioDataCreateFromFile = delegate(int _mime, int _filename, int _flags)
-//			{
-//				return MoSync.Constants.MA_AUDIO_ERR_INVALID_SOUND_FORMAT;
-//			};
+			ioctls.maAudioDataCreateFromURL = delegate(int _mime, int _url, int _flags)
+			{
+				int ret = MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+
+				try
+				{
+					String url = core.GetDataMemory().ReadStringAtAddress(_url);
+					IAudioData ad = Audio.FromUrlOrFilePath(url, (_flags & MoSync.Constants.MA_AUDIO_DATA_STREAM) != 0);
+					mAudioData.Add(ad);
+					ret = mAudioData.Count - 1;
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return ret;
+			};
 
             ioctls.maAudioDataCreateFromResource = delegate(int _mime, int _data, int _offset, int _length, int _flags)
             {
-				return MoSync.Constants.IOCTL_UNAVAILABLE;
+				int ret = MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				try
+				{
+					Resource audiores = runtime.GetResource(MoSync.Constants.RT_BINARY, _data);
+					BoundedStream s = new BoundedStream((Stream)audiores.GetInternalObject(), _offset, _length);
+					IAudioData ad = Audio.FromStream(s, (_flags & MoSync.Constants.MA_AUDIO_DATA_STREAM) != 0);
+					mAudioData.Add(ad);
+					ret =  mAudioData.Count - 1;
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return ret;
             };
 
             ioctls.maAudioDataDestroy = delegate(int _audioData)
             {
-				return MoSync.Constants.IOCTL_UNAVAILABLE;
+				try
+				{
+					IAudioData ad = mAudioData[_audioData];
+					ad.Dispose();
+					mAudioData[_audioData] = null;
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
             };
 
+			ioctls.maAudioPrepare = delegate(int _audioInstance, int async)
+			{
+				try
+				{
+					IAudioInstance ad = mAudioInstances[_audioInstance];
+					if (async == 0)
+					{
+						ad.Prepare(null);
+					}
+					else
+					{
+						ad.Prepare(() =>
+							{
+								// Send initialized event.
+								MoSync.Memory eventData = new MoSync.Memory(8);
+								eventData.WriteInt32(MoSync.Struct.MAEvent.type, MoSync.Constants.EVENT_TYPE_AUDIO_PREPARED);
+								eventData.WriteInt32(MoSync.Struct.MAEvent.audioInstance, _audioInstance);
+								runtime.PostEvent(new Event(eventData));
+							}
+						);
+					}
+					mAudioData[_audioInstance] = null;
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
+
+			};
 
             ioctls.maAudioInstanceCreate = delegate(int _audioData)
             {
-				return MoSync.Constants.IOCTL_UNAVAILABLE;
+				int ret = MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				try
+				{
+					IAudioData ad = mAudioData[_audioData];
+					mAudioInstances.Add(ad.CreateInstance());
+					ret = mAudioInstances.Count - 1;
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return ret;
             };
 
             ioctls.maAudioInstanceDestroy = delegate(int _audioInstance)
             {
-				return MoSync.Constants.IOCTL_UNAVAILABLE;
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ai.Dispose();
+					mAudioInstances[_audioInstance] = null;
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
             };
 
             ioctls.maAudioPlay = delegate(int _audioInstance)
             {
-				return MoSync.Constants.IOCTL_UNAVAILABLE;
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ai.Play();
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
             };
+
+
+			ioctls.maAudioStop = delegate(int _audioInstance)
+			{
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ai.Stop();
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
+			};
+
+			ioctls.maAudioPause = delegate(int _audioInstance)
+			{
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ai.Pause();
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
+			};
+
+			ioctls.maAudioSetNumberOfLoops = delegate(int _audioInstance, int loops)
+			{
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ai.SetNumberOfLoops(loops);
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
+			};
+
+
+			// SoundEffectInstances nor the MediaPlayer doesn't support setting position,
+			// however we can make a special case where the sound is reset if _milliseconds equals to zero.
+			// We could implement a better SoundEffectInstance using DynamicSoundEffectInstance
+			// parsing wavefiles ourselves.. But that would require some work.
+
+			ioctls.maAudioSetPosition = delegate(int _audioInstance, int _milliseconds)
+			{
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ai.SetPosition(_milliseconds);
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
+			};
+
+
+			// SoundEffectInstances doesnt support getting the location of the sound
+			// this of course could be approximated by saving a time stamp when the sound
+			// starts to play, buuut no.
+			ioctls.maAudioGetPosition = delegate(int _audioInstance)
+			{
+				int ret = MoSync.Constants.MA_AUDIO_ERR_OK;
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ret = ai.GetPosition();
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return ret;
+			};
+
+			ioctls.maAudioGetLength = delegate(int _audioInstance)
+			{
+				int ret = MoSync.Constants.MA_AUDIO_ERR_OK;
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ret = ai.GetLength();
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return ret;
+			};
+
+			ioctls.maAudioSetVolume = delegate(int _audioInstance, float volume)
+			{
+				try
+				{
+					IAudioInstance ai = mAudioInstances[_audioInstance];
+					ai.SetVolume(volume);
+				}
+				catch (MoSync.Util.ReturnValueException rve)
+				{
+					return rve.result;
+				}
+				catch (Exception)
+				{
+					return MoSync.Constants.MA_AUDIO_ERR_GENERIC;
+				}
+
+				return MoSync.Constants.MA_AUDIO_ERR_OK;
+			};
         }
     }
 }
