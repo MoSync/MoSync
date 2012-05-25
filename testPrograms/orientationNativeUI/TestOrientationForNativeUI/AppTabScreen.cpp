c9ea15b7d75adaeba96728bc8faa7517ddb77bde
Bogdan Iusco <bogdan@mosync.com>
2012-05-25 10:21:51 +0300

    Fix for MOSYNC-2101 Pointer coordintaes wrong after changing orientation on iOS

---
+++ testPrograms/orientationNativeUI/TestOrientationForNativeUI/AppTabScreen.cpp
@@ -0,0 +1,87 @@
+/*
+Copyright (C) 2012 MoSync AB
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+*/
+
+/**
+ * @file AppTabScreen.cpp
+ * @author Bogdan Iusco
+ *
+ * @brief Application's tab screen.
+ */
+
+#include <conprint.h>
+
+#include "AppTabScreen.h"
+#include "FirstScreen.h"
+#include "SecondScreen.h"
+
+namespace OrientationTest
+{
+	/**
+	 * Constructor.
+	 */
+	AppTabScreen::AppTabScreen():
+		TabScreen(),
+		mFirstScreen(NULL),
+		mSecondScreen(NULL)
+	{
+		mFirstScreen = new FirstScreen();
+		mSecondScreen = new SecondScreen();
+
+		this->addTab(mFirstScreen);
+		this->addTab(mSecondScreen);
+		this->addTabScreenListener(this);
+	}
+
+	/**
+	 * Destructor.
+	 */
+	AppTabScreen::~AppTabScreen()
+	{
+		this->removeTabScreenListener(this);
+	}
+
+	/**
+	 * This method is called when a tab screen has changed to a new tab.
+	 * @param tabScreen The tab screen object that generated the event.
+	 * @param tabScreenIndex The index of the new tab.
+	 */
+	void AppTabScreen::tabScreenTabChanged(
+		TabScreen* tabScreen,
+		const int tabScreenIndex)
+	{
+
+	}
+
+	/**
+	 * Called just before the screen begins rotating.
+	 */
+	void AppTabScreen::orientationWillChange()
+	{
+		mFirstScreen->orientationWillChange();
+		mSecondScreen->orientationWillChange();
+	}
+
+	/**
+	 * Called after the screen orientation has changed (available only on Windows Phone 7.1 platform
+	 */
+	void AppTabScreen::orientationDidChange()
+	{
+
+	}
+
+} //namespace OrientationTest
