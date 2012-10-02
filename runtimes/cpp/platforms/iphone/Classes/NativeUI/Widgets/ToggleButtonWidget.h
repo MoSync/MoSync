a1538e026fa3b5f3d582cc9ef9fd5d2d0a0a9b48
Bogdan Iusco <bogdan@mosync.com>
2012-10-02 10:09:12 +0300

    Jira task CJLO-42 "Add toggle button on iOS."

---
+++ runtimes/cpp/platforms/iphone/Classes/NativeUI/Widgets/ToggleButtonWidget.h
@@ -0,0 +1,13 @@
+//
+//  ToggleButtonWidget.h
+//  MoSync
+//
+//  Created by Bogdan Iusco on 9/27/12.
+//
+//
+
+#import "IWidget.h"
+
+@interface ToggleButtonWidget : IWidget
+
+@end
---
+++ runtimes/cpp/platforms/iphone/Classes/NativeUI/Widgets/ToggleButtonWidget.mm
@@ -0,0 +1,13 @@
+//
+//  ToggleButtonWidget.mm
+//  MoSync
+//
+//  Created by Bogdan Iusco on 9/27/12.
+//
+//
+
+#import "ToggleButtonWidget.h"
+
+@implementation ToggleButtonWidget
+
+@end
