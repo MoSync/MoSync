065a4f29cde90600987a0bdc12ac9b8be3f8e6d2
Spiridon Alexandru <spiridon.g.alex@gmail.com>
2013-02-26 11:17:17 +0200

    MOSYNC-2856: fixed - on back button event we now check if the stack is empty or not. If empty, the app will close (nothing happens otherwise). + small code review fixes

--- runtimes/csharp/windowsphone/mosync/mosyncRuntime/Source/Modules/MoSyncNativeUIModule.cs
+++ runtimes/csharp/windowsphone/mosync/mosyncRuntime/Source/Modules/MoSyncNativeUIModule.cs
@@ -31,7 +31,7 @@ namespace MoSync
             {
                 return mCurrentScreen.HandleBackButtonPressed();
             }
-            return true;
+            return false;
         }
 
         /*
