ec8645f4efe66a1de8f269d821ef6a9968b93938
Fredrik Eldh <fredrik.eldh@mobilesorcery.com>
2013-03-26 13:48:50 +0100

    WindowsPhone7: Implemented maSound syscalls with MP3 support. Added library ManagedMediaHelpers. Next step: add MP3 support to maAudio.

---
+++ runtimes/csharp/windowsphone/ManagedMediaHelpers/Mp3MediaStreamSourceDemo.Phone/App.xaml.cs
@@ -0,0 +1,172 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Larry Olson">
+// (c) Copyright Larry Olson.
+// This source is subject to the Microsoft Public License (Ms-PL)
+// See http://code.msdn.microsoft.com/ManagedMediaHelpers/Project/License.aspx
+// All other rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Mp3MediaStreamSourceWP7Demo
+{
+    using System.Windows;
+    using System.Windows.Navigation;
+    using Microsoft.Phone.Controls;
+    using Microsoft.Phone.Shell;
+
+    /// <summary>
+    /// The application
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Avoid double-initialization 
+        /// </summary>
+        private bool phoneApplicationInitialized = false;
+
+        /// <summary>
+        /// Initializes a new instance of the App class.
+        /// </summary>
+        public App()
+        {
+            // Global handler for uncaught exceptions. 
+            UnhandledException += this.Application_UnhandledException;
+
+            // Show graphics profiling information while debugging.
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // Display the current frame rate counters.
+                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+
+                // Show the areas of the app that are being redrawn in each frame.
+                // Application.Current.Host.Settings.EnableRedrawRegions = true;
+
+                // Enable non-production analysis visualization mode, 
+                // which shows areas of a page that are being GPU accelerated with a colored overlay.
+                // Application.Current.Host.Settings.EnableCacheVisualization = true;
+            }
+
+            // Standard Silverlight initialization
+            InitializeComponent();
+
+            // Phone-specific initialization
+            this.InitializePhoneApplication();
+        }
+
+        /// <summary>
+        /// Gets the the root frame of the Phone Application.
+        /// </summary>
+        /// <returns>The root frame of the Phone Application.</returns>
+        public PhoneApplicationFrame RootFrame { get; private set; }
+
+        /// <summary>
+        /// Code to execute when the application is launching (eg, from Start) 
+        /// This code will not execute when the application is reactivated
+        /// </summary>
+        /// <param name="sender">the sender</param>
+        /// <param name="e">the event args</param>
+        private void Application_Launching(object sender, LaunchingEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Code to execute when the application is activated (brought to foreground)
+        /// This code will not execute when the application is first launched
+        /// </summary>
+        /// <param name="sender">the sender</param>
+        /// <param name="e">the event args</param>
+        private void Application_Activated(object sender, ActivatedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Code to execute when the application is deactivated (sent to background)
+        /// This code will not execute when the application is closing
+        /// </summary>
+        /// <param name="sender">the sender</param>
+        /// <param name="e">the event args</param>
+        private void Application_Deactivated(object sender, DeactivatedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Code to execute when the application is closing (eg, user hit Back)
+        /// This code will not execute when the application is deactivated
+        /// </summary>
+        /// <param name="sender">the sender</param>
+        /// <param name="e">the event args</param>
+        private void Application_Closing(object sender, ClosingEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Code to execute if a navigation fails
+        /// </summary>
+        /// <param name="sender">the sender</param>
+        /// <param name="e">the event args</param>
+        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // A navigation has failed; break into the debugger
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        /// <summary>
+        /// Code to execute on Unhandled Exceptions 
+        /// </summary>
+        /// <param name="sender">the sender</param>
+        /// <param name="e">the event args</param>
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        #region Phone application initialization
+
+        /// <summary>
+        /// Do not add any additional code to this method
+        /// </summary>
+        private void InitializePhoneApplication()
+        {
+            if (this.phoneApplicationInitialized)
+            {
+                return;
+            }
+
+            // Create the frame but don't set it as RootVisual yet; this allows the splash
+            // screen to remain active until the application is ready to render.
+            this.RootFrame = new PhoneApplicationFrame();
+            this.RootFrame.Navigated += this.CompleteInitializePhoneApplication;
+
+            // Handle navigation failures
+            this.RootFrame.NavigationFailed += this.RootFrame_NavigationFailed;
+
+            // Ensure we don't initialize again
+            this.phoneApplicationInitialized = true;
+        }
+
+        /// <summary>
+        /// Do not add any additional code to this method 
+        /// </summary>
+        /// <param name="sender">the sender</param>
+        /// <param name="e">the event args</param>
+        private void CompleteInitializePhoneApplication(object sender, NavigationEventArgs e)
+        {
+            // Set the root visual to allow the application to render
+            if (RootVisual != this.RootFrame)
+            {
+                RootVisual = this.RootFrame;
+            }
+
+            // Remove this handler since it is no longer needed
+            this.RootFrame.Navigated -= this.CompleteInitializePhoneApplication;
+        }
+
+        #endregion
+    }
+}
