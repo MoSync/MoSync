9e899988a61a18c4fa17347748bfdb67935295ff
Mattias Bybro <mattias.bybro@mosync.com>
2013-04-02 08:42:43 +0200

    Revert "MOSYNC-2742: First minimal commit for iOS extensions"
    This reverts commit 9eaa67a7ac2f8444e38329e6acb22d93b1d179ae.
    
    Conflicts:
    
    	runtimes/cpp/platforms/iphone/Classes/impl/SyscallImpl.mm

--- runtimes/cpp/platforms/iphone/Classes/Extensions/MoSyncExtension.h
+++ runtimes/cpp/platforms/iphone/Classes/Extensions/MoSyncExtension.h
@@ -2,7 +2,6 @@
 #define _MOSYNC_EXTENSION_PROTOCOL_H_
 
 #include "MoSync.h"
-#include "cpp_maapi.h"
 
 @protocol MoSyncExtensionProtocol
 	-(id) initWithMoSync:(MoSync*)mosync;
@@ -12,14 +11,6 @@
 	-(long long) invokeExtensionWithIndex:(int)id andArg1:(int)a andArg2:(int)b andArg3:(int)c;
 @end
 
-@interface FunctionInvocation : NSObject
--(id) init;
--(void) invoke:(int)numargs (int)ptr;
-@end
-
 void initExtensions(MoSync* mosync);
-MAExtensionModule getExtensionModule(const char* name, int hash);
-MAExtensionFunction getExtensionFunction(MAExtensionModule module, int ix);
-int invokeExtensionFunction(MAExtensionFunction fn, int numargs, int ptrs);
 
 #endif // _MOSYNC_EXTENSION_PROTOCOL_H_
--- runtimes/cpp/platforms/iphone/Classes/Extensions/MoSyncExtension.mm
+++ runtimes/cpp/platforms/iphone/Classes/Extensions/MoSyncExtension.mm
@@ -2,72 +2,27 @@
 #include "MoSyncExtension.h"
 #include <helpers/cpp_defs.h>
 
-static NSMutableDictionary* sExtensions;
-static NSMutableDictionary* sFunctions;
+static NSMutableArray* sExtensions;
 
 void initExtensions(MoSync* mosync)
 {
-	sExtensions = [[NSMutableDictionary alloc] init ];
+	sExtensions = [[NSMutableArray alloc] init ];
 	int numClasses = objc_getClassList(NULL, 0);
 	Class *classes = NULL;
 	classes = (Class*)malloc(sizeof(Class) * numClasses);
 	numClasses = objc_getClassList(classes, numClasses);
 
-    id object = [[NSClassFromString(extensionImpl) alloc] init];
 	for(int i = 0; i < numClasses; i++)
 	{
 		if(classes[i] && class_conformsToProtocol(classes[i], @protocol(MoSyncExtensionProtocol)))
 		{
 			id extension = [[classes[i] alloc] initWithMoSync:mosync];
-			[sExtensions setObject:extension forKey:extensionName];
+			[sExtensions addObject:extension];
 		}
 	}
 
 	free((void*) classes);
 }
-
-int invokeExtensionFunction(MAExtensionFunction fn, int numargs, int ptrs) {
-    FunctionInvocation* fn = [sFunctions objectForKey:fn]
-    if (fn) {
-        fn->invoke(numargs, ptrs);
-        return 0;
-    }
-    maPanic(40035, "Cannot find function");
-    return 1;
-}
-
-@interface ExtensionManager : NSObject {
-}
-
--(void) load;
--(int) maExtensionModuleLoad:NSString name :int hash;
--(int) maExtensionFunctionLoad:int module :int fn;
--(int) invoke:(int)fn ;
-
-@end
-
-@implementation ExtensionManager
--(int) maExtensionModuleLoad:NSString name, int hash
-{
-}
--(int) maExtensionFunctionLoad:int module,int fn
-{
-
-}
-
-@end
-
-@implementation FunctionInvocation
--(id) init
-{
-    return self;
-}
-
--(void) invoke:(int)numargs (int) ptrs
-{
-    (int*) castPtrs = (int*)ptrs;
-}
-@end
 
 #if 0
 @interface TestExtension : NSObject<MoSyncExtensionProtocol>
