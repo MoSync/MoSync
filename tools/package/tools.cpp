849d60544010cff71900f3fa46e0709a4c82ae2d
Mattias Bybro <mattias.bybro@mosync.com>
2013-04-02 10:48:22 +0200

    MOSYNC-2737: Fixed import in package

--- tools/package/tools.cpp
+++ tools/package/tools.cpp
@@ -19,6 +19,7 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #include "tools.h"
 #include "package.h"
 #include <string>
+#include <cstring>
 #include <sstream>
 #include <ostream>
 
