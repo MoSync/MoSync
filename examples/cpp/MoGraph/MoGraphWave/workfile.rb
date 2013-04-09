a320e1772191c0a3ff8414714140ffc163d16ffa
robert <robertg71@yahoo.se>
2013-04-09 11:17:00 +0200

    Added first example for MoGraph lib MoGraphWave

--- examples/cpp/MoGraph/MoGraphWave/workfile.rb
+++ examples/cpp/MoGraph/MoGraphWave/workfile.rb
@@ -8,6 +8,7 @@ work.instance_eval do
 	@EXTRA_INCLUDES = ["#{mosyncdir}/include/newlib/stlport", "#{mosyncdir}/include/newlib/glm", "#{mosyncdir}/include/newlib/MoGraph"]
 	@EXTRA_CPPFLAGS = ' -Wno-float-equal -Wno-unreachable-code -Wno-shadow -Wno-missing-noreturn'
 	@LIBRARIES = ["stlport", "mautil", "mograph"]
+	@EXTRA_LINKFLAGS = ' -datasize=2048000 -heapsize=1728000 -stacksize=128000'
 	@NAME = "MoGraphWave"
 end
 
