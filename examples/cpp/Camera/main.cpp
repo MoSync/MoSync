e7c398911fdba6ed4c2a82bad508e545a6ed7c1e
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2013-04-10 11:59:00 +0300

    Update main.cpp

--- examples/cpp/Camera/main.cpp
+++ examples/cpp/Camera/main.cpp
@@ -16,7 +16,7 @@ namespace MoSyncCamera
 	class NativeUIMoblet : public Moblet
 	{
 	public:
-		/**
+		/* FIXME: remove this
 		 * The constructor creates the user interface.
 		 */
 		NativeUIMoblet()
@@ -26,7 +26,7 @@ namespace MoSyncCamera
 			mAppController->displayMainScreen();
 		}
 
-		/**
+		/* FIXME: remove this
 		 * Destructor.
 		 */
 		virtual ~NativeUIMoblet()
@@ -34,7 +34,7 @@ namespace MoSyncCamera
 			delete mAppController;
 		}
 
-		/**
+		/* FIXME: remove this comment
 		 * Called when a key is pressed.
 		 */
 		void keyPressEvent(int keyCode, int nativeCode)
@@ -47,18 +47,32 @@ namespace MoSyncCamera
 		}
 
 	private:
-		/**
+		/* FIXME: remove this comment
 		 * Application controller
 		 */
-		MoSyncCamController* mAppController;
+		MoSyncCamController* mAppController;	// FIXME: very dangerous naming, keep camera controller
 	};
 } // CameraDemo
 
-/**
+/* FIXME: remove this
  * Main function that is called when the program starts.
  */
 extern "C" int MAMain()
 {
+	// FIXME: this is a bad habbit which should not be promoted by MoSync developers.
+	// it's one line of code which does almost everything together with a leak - even if we rely on OS for cleanup.
 	Moblet::run(new MoSyncCamera::NativeUIMoblet());
+	
+	// ALTERNATIVE:
+	MoSyncCamera::NativeUIMoblet *cameraMoblet = new MoSyncCamera::NativeUIMoblet();
+	
+	// here you have the opportunity to check if construction went well
+	
+	Moblet::run( cameraMoblet );
+	
+	// here you have the oppotunity to release objects, timers and so on... aka clean-up
+	delete cameraMoblet;
+	
+	// and only now have a gracefull exit.
 	return 0;
 }
