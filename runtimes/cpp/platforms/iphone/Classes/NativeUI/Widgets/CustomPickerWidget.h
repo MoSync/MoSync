f9247cab69429604adde9cde586794f14fe9c2df
Bogdan Iusco <bogdan@mosync.com>
2013-04-16 14:11:39 +0300

    MOSYNC-3090 Implement CustomPicker properties on iOS platoform

--- runtimes/cpp/platforms/iphone/Classes/NativeUI/Widgets/CustomPickerWidget.h
+++ runtimes/cpp/platforms/iphone/Classes/NativeUI/Widgets/CustomPickerWidget.h
@@ -60,4 +60,23 @@
  */
 - (void)removeChild:(IWidget*)child;
 
+/**
+ * Set a widget property value.
+ * @param key Widget's property name that should be set.
+ * @param value Widget's proeprty value that should be set.
+ * @return One of the following values:
+ * - MAW_RES_OK if the property was set.
+ * - MAW_RES_INVALID_PROPERTY_NAME if the property name was invalid.
+ * - MAW_RES_INVALID_PROPERTY_VALUE if the property value was invalid.
+ */
+- (int)setPropertyWithKey:(NSString*)key toValue:(NSString*)value;
+
+/**
+ * Get a widget property value.
+ * @param key Widget's property name.
+ * @return The property value, or nil if the property name is invalid.
+ * The returned value should not be autoreleased. The caller will release the returned value.
+ */
+- (NSString*)getPropertyWithKey:(NSString*)key;
+
 @end
