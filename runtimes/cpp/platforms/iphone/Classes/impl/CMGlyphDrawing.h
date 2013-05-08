a7a9b479d5d1790f97c7f299c9264274388e1c05
Bogdan Iusco <bogdan@mosync.com>
2013-05-08 15:18:27 +0300

    MOSYNCLIB-59 printf doesn't work properly with Native binary settings on iOS simulator

--- runtimes/cpp/platforms/iphone/Classes/impl/CMGlyphDrawing.h
+++ runtimes/cpp/platforms/iphone/Classes/impl/CMGlyphDrawing.h
@@ -2,7 +2,9 @@
 //  CMGlyphDrawing.h
 //
 
-//#import <Foundation/Foundation.h>
 #import <QuartzCore/QuartzCore.h>
 
 extern int CMFontGetGlyphsForUnichars(CGFontRef font, const UniChar buffer[], CGGlyph glyphs[], size_t numGlyphs);
+
+// temporary fix for native library approach
+extern int CMFontGetGlyphsForUnichars(CGFontRef font, const UInt32 buffer[], CGGlyph glyphs[], size_t numGlyphs);
