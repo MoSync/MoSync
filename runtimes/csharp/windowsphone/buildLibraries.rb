cdfce5aaae3808154afd2475e4e0080e7caa657c
Ciprian Filipas <cipri@mosync.com>
2013-05-14 15:28:22 +0300

    separated the buildLibraries files , one is for WP7 one for WP8

--- runtimes/csharp/windowsphone/buildLibraries.rb
+++ runtimes/csharp/windowsphone/buildLibraries.rb
@@ -24,29 +24,15 @@ msbuildpath = "/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe"
 
 # build everything
 sh "#{msbuildpath} mosync/mosyncRuntime/mosyncRuntime.csproj"
-sh "#{msbuildpath} mosync/mosyncRuntime_solution_WP8/mosyncRuntime_WP8.sln"
 
 # build directory structure
 FileUtils.rm_rf(["template"])
-FileUtils.rm_rf(["template_WP8"])
 FileUtils.mkpath(["template/Libraries", "template/Libraries/mosyncRuntime", "template/RebuildData"])
-FileUtils.mkpath(["template_WP8/Libraries", "template_WP8/mosyncRuntime", "template_WP8/mosync_WP8", "template_WP8/MoSyncLibrary"])
-FileUtils.mkpath(["template_WP8/Libraries/Media", "template_WP8/Libraries/Media/debug_lib_build"])
-FileUtils.mkpath(["template_WP8/packages"])
 
 FileUtils.cp_r Dir.glob('mosync/mosyncRuntime/Bin/Release/*.*'), 'template/Libraries/mosyncRuntime', :verbose => true
 FileUtils.cp_r Dir.glob('mosync/mosyncRuntime/Bin/Debug/*.*'), 'template/Libraries/mosyncRuntime', :verbose => true
 FileUtils.cp_r Dir.glob('mosync/mosync/*.*'), 'template', :verbose => true
-FileUtils.cp_r Dir.glob('mosync/Libraries/*.*'), 'template_WP8/Libraries', :verbose => true
-FileUtils.cp_r Dir.glob('mosync/mosync_WP8/*.*'), 'template_WP8/mosync_WP8', :verbose => true
-FileUtils.cp_r Dir.glob('mosync/Libraries/Media/debug_lib_build/*.*'), 'template_WP8/Libraries/Media/debug_lib_build', :verbose => true
-FileUtils.cp_r Dir.glob('mosync/MoSyncLibrary/*.*'), 'template_WP8/MoSyncLibrary', :verbose => true
 FileUtils.cp_r Dir.glob('mosync/mosync/Properties'), 'template', :verbose => true
-FileUtils.cp_r Dir.glob('mosync/mosync_WP8/Properties'), 'template_WP8/mosync_WP8', :verbose => true
-FileUtils.cp_r Dir.glob('mosync/mosync_WP8/Toolkit.Content'), 'template_WP8/mosync_WP8', :verbose => true
-FileUtils.cp_r Dir.glob('mosync/packages/*.*'), 'template_WP8/packages', :verbose => true
-
-sh "xcopy \"mosync/mosyncRuntime\" \"template_WP8/mosyncRuntime\" /S /I /EXCLUDE:xcopyExcludes.txt"
 
 #sh "winphone-builder -input-file template/mosync.csproj -output-type rebuilt -output-file template/mosync_rebuilt.csproj"
 #sh "winphone-builder -input-file template/mosync.csproj -output-type interpreted -output-file template/mosync_interpreted.csproj"
@@ -57,10 +43,8 @@ FileUtils.mkpath([
 					mosyncdir+"/profiles/runtimes/winphone",
 					mosyncdir+"/profiles/runtimes/winphone/1",
 					mosyncdir+"/profiles/runtimes/winphone/1/template",
-					mosyncdir+"/profiles/runtimes/winphone/1/template_WP8",
 					mosyncdir+"/profiles/runtimes/winphone/1/extension_dev_template"])
 FileUtils.cp_r "template", mosyncdir+"/profiles/runtimes/winphone/1"
-FileUtils.cp_r "template_WP8", mosyncdir+"/profiles/runtimes/winphone/1"
 
 FileUtils.cp_r "mosync/mosyncExtensionTemplate", mosyncdir+"/profiles/runtimes/winphone/1"
 FileUtils.cp_r "mosync/mosyncExtensionTemplate", mosyncdir+"/profiles/runtimes/winphone/1"
