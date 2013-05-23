f9c47bb28d575e26561bc3df90f4405901d4d823
Ciprian Filipas <cipri@mosync.com>
2013-05-23 16:10:54 +0300

    this was required in order to avoid a link error regarding strtol

--- libs/MAStd/mastdlib.h
+++ libs/MAStd/mastdlib.h
@@ -74,7 +74,9 @@ int axtoi(const char *hexStg);
 /*
  * Convert a string to a long integer.
  */
+#ifndef WINDOWS_PHONE_8
 long strtol(const char *nptr, char **endptr, int base);
+#endif
 
 /*
  * Convert a string to an unsigned long integer.
